1a55da483779993d89576e1c20b86dbd82357755
Will Mayner <wmayner@gmail.com>
2014-01-21 12:23:16 -0600

    Use normal Python for models (for now)

--- cyphi/models.py
+++ cyphi/models.py
@@ -9,25 +9,11 @@ This module contains the primary objects that power CyPhi.
 
 """
 
-# NOTE: ``cimport`` is used to import special compile-time
-# information about the numpy module (this is stored in a file numpy.pxd which
-# is currently part of the Cython distribution, not the actualy numpy module
-# itself; hence the strange almost-duplicate lines below.
 import numpy as np
-cimport numpy as np
-
 import itertools.chain
 import utils
 
-# Fix a datatype for numpy arrays.
-DTYPE = np.int
-# ``ctypedef`` assigns a corresponding compile-time type to DTYPE_t. For every
-# type in the numpy module there's a corresponding compile-time type with a
-# _t-suffix.
-ctypedef np.int_t DTYPE_t
-
-
-cdef class Network(object):
+def class Network(object):
     """A network of elements.
 
     Represents the network we're analyzing and holds auxilary data about it.
@@ -42,17 +28,14 @@ cdef class Network(object):
 
     """
 
-    # Attributes of Cython "extension class" (classes defined with ``cdef``)
-    # are defined outside of the constructor (the ``__cinit__`` function),
-    # unlike regular Python
-    cdef public np.ndarray connectivity_matrix, tpm
-    cdef public itertools.chain powerset
-
-    def __cinit__(self, connectivity_matrix, tpm):
+    def __init__(self, connectivity_matrix, tpm):
+        # Ensure connectivity matrix is square
         if len(connectivity_matrix.shape) is not 2 or \
             connectivity_matrix.shape[0] is not connectivity_matrix.shape[1]:
             raise utils.ValidationException("Connectivity matrix must be square.")
+
         self.connectivity_matrix = connectivity_matrix
         self.tpm = tpm
+
         # Generate powerset
         self.powerset = utils.powerset(np.arange(connectivity_matrix.shape[0]))
