d63f8655514b32af36a90277f1d5f8ce6915ae9b
Will Mayner <wmayner@gmail.com>
2014-04-07 17:18:18 -0500

    Don't need Cython yet

--- cyphi/__init__.py
+++ cyphi/__init__.py
@@ -15,14 +15,8 @@ __author_email__ = 'wmayner@gmail.com'
 __copyright__ = 'Copyright 2014 Will Mayner'
 
 
-# Import Cython modules like normal Python modules (automatically compile them)
-# NOTE: this requires the environment to have Cython; compile and bundle
-# everything explicitly before distributing
 import numpy as np
-import pyximport
-pyximport.install(setup_args={"include_dirs": np.get_include()})
 
-from .node import Node
 from .network import Network
 from .subsystem import Subsystem
 from . import utils
