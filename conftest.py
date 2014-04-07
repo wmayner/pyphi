e4a6a80ae10a28bcefa60e0bb8efe8b6fff03129
Will Mayner <wmayner@gmail.com>
2014-04-07 15:52:26 -0500

    Add command-line option to separate slow tests

--- conftest.py
+++ conftest.py
@@ -9,6 +9,10 @@ import test.example_networks
 collect_ignore = ["setup.py", ".pythonrc.py"]
 
 
+# Test fixtures from example networks
+# ===================================
+
+
 @pytest.fixture()
 def m():
     return test.example_networks.m()
@@ -17,3 +21,16 @@ def m():
 @pytest.fixture()
 def s():
     return test.example_networks.s()
+
+
+# Run slow tests separately with command-line option
+# ==================================================
+
+
+def pytest_addoption(parser):
+    parser.addoption("--runslow", action="store_true", help="run slow tests")
+
+
+def pytest_runtest_setup(item):
+    if 'slow' in item.keywords and not item.config.getoption("--runslow"):
+        pytest.skip("need --runslow option to run")
