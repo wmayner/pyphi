49590d31173a506b02390bb1fee839f2b1e2326d
Will Mayner <wmayner@gmail.com>
2014-04-07 18:54:07 -0500

    Polish setup.py

--- cyphi/__init__.py
+++ cyphi/__init__.py
@@ -5,22 +5,23 @@
 CyPhi
 =====
 
-CyPhi is a Cython library for computing integrated information.
+CyPhi is a Python library for computing integrated information.
 """
 
 __title__ = 'cyphi'
 __version__ = '0.0.1'
+__description__ = 'Python library for computing integrated information.',
 __author__ = 'Will Mayner'
 __author_email__ = 'wmayner@gmail.com'
+__author_website__ = 'http://willmayner.com'
 __copyright__ = 'Copyright 2014 Will Mayner'
 
-
-import numpy as np
 
 from .network import Network
 from .subsystem import Subsystem
 from . import utils
 from . import constants
+
 
 # TODO Optimizations:
 # - Memoization
