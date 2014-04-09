88da59a1a2700bfa48f28db6ca95a59cd6e95fbc
Will Mayner <wmayner@gmail.com>
2014-04-09 14:30:10 -0500

    Fix install_requires and don't import the module in setup

--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import cyphi
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-packages = ['cyphi']
-
-requires = ['numpy==1.8.1', 'scipy==0.13.3']
 
 with open('README.rst') as f:
     readme = f.read()
 
+
+requires = ['numpy >=1.8.1, <2.0.0',
+            'scipy >=0.13.3, <1.0.0',
+            'git+https://github.com/wmayner/pyemd#egg=pyemd']
+
+
 setup(
     name="cyphi",
-    version=cyphi.__version__,
-    author=cyphi.__author__,
-    author_email=cyphi.__author_email__,
-    description=cyphi.__description__,
+    version='0.0.2',
+    description='Python library for computing integrated information.',
+    author='Will Mayner',
+    author_email='wmayner@gmail.com',
     long_description=readme,
     include_package_data=True,
     install_requires=requires,
-    packages=packages,
-    package_data={'': ['LICENSE', 'NOTICE']},
+    packages=['cyphi'],
+    package_data={'': ['LICENSE']},
     license='GNU General Public License v3.0',
     zip_safe=False,
     classifiers=(
@@ -37,5 +39,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering'
     ),
 )
