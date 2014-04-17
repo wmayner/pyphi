faeed462cdc594163930594a2b04ef200fdaad67
Will Mayner <wmayner@gmail.com>
2014-04-17 18:04:48 -0500

    Finish modifying Network for connectivity matrix
    - Update comparison and hash methods to take connectivity matrix into
      account
    - Check if connectivity matrix is None before setting flags

--- cyphi/network.py
+++ cyphi/network.py
@@ -9,7 +9,6 @@ Represents the network of interest. This is the primary object of CyPhi and the
 context of all |phi| and |big_phi| computation.
 """
 
-
 import numpy as np
 from .node import Node
 from .subsystem import Subsystem
@@ -41,6 +40,8 @@ class Network:
             ``state[i]`` gives the past state of ``self.nodes[i]``
         :type state: ``np.ndarray``
         """
+        # TODO make tpm also optional when implementing logical network
+        # definition
         self.tpm = tpm
         # TODO! test connectivity matrix
         self.connectivity_matrix = connectivity_matrix
@@ -50,6 +51,8 @@ class Network:
         self.tpm.flags.writeable = False
         self.current_state.flags.writeable = False
         self.past_state.flags.writeable = False
+        if self.connectivity_matrix != None:
+            self.connectivity_matrix.flags.writeable = False
         # The number of nodes in the Network (TPM is in state-by-node form, so
         # number of nodes is given by the size of the last dimension)
         self.size = tpm.shape[-1]
@@ -64,24 +67,33 @@ class Network:
         self.num_states = 2 ** self.size
 
     def __repr__(self):
-        return "Network(" + repr(self.tpm) + ")"
+        return ("Network(" + ", ".join([repr(self.tpm),
+                                          repr(self.current_state),
+                                          repr(self.past_state)]) +
+                ", connectivity_matrix=" + repr(self.connectivity_matrix) + ")")
 
     def __str__(self):
-        return "Network(" + str(self.tpm) + ")"
+        return ("Network(" + str(self.tpm) + ", connectivity_matrix=" +
+                str(self.connectivity_matrix) + ")")
 
     def __eq__(self, other):
         """Two networks are equal if they have the same TPM, current state, and
         past state."""
         return (np.array_equal(self.tpm, other.tpm) and
                 np.array_equal(self.current_state, other.current_state) and
-                np.array_equal(self.past_state, other.past_state))
+                np.array_equal(self.past_state, other.past_state) and
+                np.array_equal(self.connectivity_matrix, other.connectivity_matrix))
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
+    # TODO don't use tostring(not unique for large arrays)
     def __hash__(self):
-        return hash((self.tpm.tostring(), self.current_state.tostring(),
-                    self.past_state.tostring()))
+        return hash((self.tpm.tostring(),
+                     self.current_state.tostring(),
+                     self.past_state.tostring(),
+                     (self.connectivity_matrix.tostring() if
+                      self.connectivity_matrix != None else None)))
 
     def subsystems(self):
         """Return a generator of all possible subsystems of this network."""
