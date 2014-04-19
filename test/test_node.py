1c1cff0365bd1338fc5f4210d81b78f1ec333225
Will Mayner <wmayner@gmail.com>
2014-04-19 18:47:34 -0500

    Add more node initialization tests

--- test/test_node.py
+++ test/test_node.py
@@ -5,12 +5,37 @@ import numpy as np
 
 from cyphi.network import Network
 from cyphi.node import Node
+from cyphi import utils
 
 
-def test_node_init(m):
-    node_tpm = m.nodes[2].tpm
-    answer = m.tpm[:, :, :, 2]
-    assert np.array_equal(node_tpm, answer)
+def test_node_init_tpm(m):
+    answer = [
+        np.array([[[[1., 0.],
+                    [0., 0.]]],
+                  [[[0., 1.],
+                    [1., 1.]]]]),
+        np.array([[[[1., 0.]]],
+                  [[[0., 1.]]]]),
+        np.array([[[[1.],
+                    [0.]],
+                   [[0.],
+                    [1.]]],
+                  [[[0.],
+                    [1.]],
+                   [[1.],
+                    [0.]]]])
+    ]
+    for node in m.nodes:
+        assert np.array_equal(node.tpm, answer[node.index])
+
+def test_node_init_inputs(m):
+    answer = [
+        m.nodes[1:],
+        m.nodes[2:3],
+        m.nodes[:2]
+    ]
+    for node in m.nodes:
+        assert node.inputs == set(answer[node.index])
 
 
 def test_node_eq(m):
@@ -22,5 +47,5 @@ def test_node_neq_by_index(m):
 
 
 def test_node_neq_by_network(m):
-    other_network = Network(m.tpm, np.array([0, 0, 0]), m.past_state)
+    other_network = Network(m.tpm, (0, 0, 0), (0, 0, 0))
     assert m.nodes[0] != Node(other_network, 0)
