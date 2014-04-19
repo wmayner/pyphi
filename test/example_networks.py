5b64326f95d185a7b99fc31a62dff2676c855587
Will Mayner <wmayner@gmail.com>
2014-04-19 18:25:52 -0500

    Examples: using tuples for state

--- test/example_networks.py
+++ test/example_networks.py
@@ -58,20 +58,21 @@ def m():
 
     """
     # TODO? make these into dictionaries/named tuples
-    current_state = np.array([1, 0, 0])
-    past_state = np.array([1, 1, 0])
+    current_state = (1, 0, 0)
+    past_state = (1, 1, 0)
     tpm = np.array([[0, 0, 0],
-                    [0, 0, 1],
-                    [1, 0, 1],
-                    [1, 0, 0],
                     [1, 1, 0],
+                    [1, 0, 1],
                     [1, 1, 1],
+                    [0, 0, 1],
                     [1, 1, 1],
-                    [1, 1, 0]]).reshape([2] * 3 + [3], order="F").astype(float)
+                    [1, 0, 0],
+                    [1, 1, 0]])
 
     cm = np.array([[0, 0, 1],
                    [1, 0, 1],
                    [1, 1, 0]])
+
     cm = None if no_connectivity else cm
     m = Network(tpm, current_state, past_state, connectivity_matrix=cm)
     m.subsys_n0n2 = Subsystem([m.nodes[0], m.nodes[2]],
@@ -116,8 +117,8 @@ def s():
     |  {1, 1, 1}   |   {0, 0, 0}    |
     +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
     """
-    a_just_turned_on = np.array([1, 0, 0])
-    a_about_to_be_on = np.array([0, 1, 1])
+    a_just_turned_on = (1, 0, 0)
+    a_about_to_be_on = (0, 1, 1)
     tpm = np.array([[0, 0, 0],
                     [0, 0, 0],
                     [0, 0, 0],
@@ -141,65 +142,56 @@ def s():
                                        s)
     # Subsystem([n0, n1, n2]) of the simple 'AND' network.
     # All nodes are off.
-    s.subsys_all_off = Subsystem(s.nodes,
-                                 np.array([0, 0, 0]),
-                                 np.array([0, 0, 0]),
-                                 s)
+    s.subsys_all_off = Subsystem(s.nodes, (0, 0, 0), (0, 0, 0), s)
     return s
 
 
 def big():
-    """ Large network.
-
-    TPM:
-
-
-    """
+    """Return a large network."""
     tpm = np.array([[0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
-                    [0, 0, 1, 1, 0],
+                    [1, 1, 0, 0, 0],
                     [0, 0, 0, 0, 0],
-                    [0, 0, 0, 1, 0],
-                    [0, 0, 0, 1, 1],
-                    [0, 0, 1, 1, 1],
-                    [0, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 0],
-                    [0, 0, 0, 0, 1],
-                    [0, 1, 1, 1, 1],
+                    [1, 0, 0, 0, 0],
                     [1, 0, 0, 0, 1],
-                    [1, 1, 0, 1, 1],
+                    [1, 1, 0, 0, 1],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 1],
+                    [1, 1, 1, 0, 1],
+                    [0, 0, 0, 1, 1],
+                    [1, 0, 1, 1, 1],
                     [1, 0, 0, 1, 1],
                     [1, 1, 1, 1, 1],
                     [0, 0, 0, 0, 0],
                     [0, 1, 1, 0, 0],
-                    [0, 0, 1, 0, 0],
-                    [0, 1, 1, 1, 0],
-                    [1, 0, 0, 0, 0],
-                    [1, 1, 1, 1, 0],
-                    [1, 0, 1, 1, 1],
-                    [1, 1, 1, 1, 1],
-                    [1, 1, 0, 0, 0],
+                    [0, 1, 0, 0, 0],
                     [1, 1, 1, 0, 0],
-                    [1, 1, 1, 0, 1],
+                    [0, 0, 0, 1, 0],
+                    [1, 1, 1, 1, 0],
+                    [1, 1, 0, 1, 1],
                     [1, 1, 1, 1, 1],
-                    [1, 1, 0, 0, 1],
+                    [0, 0, 1, 1, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1],
+                    [0, 0, 1, 1, 1],
                     [1, 1, 1, 1, 1],
                     [1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1]]).reshape([2] * 5 + [5],
-                                              order="F").astype(float)
+                    [1, 1, 1, 1, 1]]).reshape([2] * 5 + [5]).astype(float)
     # All on
-    current_state = np.array([1] * 5)
+    current_state = (1,) * 5
     # All on
-    past_state = np.array([1] * 5)
+    past_state = (1,) * 5
     big = Network(tpm, current_state, past_state)
     big.subsys_all = Subsystem(big.nodes, current_state, past_state, big)
     return big
 
 def reducible():
     tpm = np.zeros([2] * 2 + [2])
-    current_state = np.zeros(2)
-    past_state = np.zeros(2)
+    current_state = (0, 0)
+    past_state = (0, 0)
     cm = np.array([[1, 0],
                    [0, 1]])
     cm = None if no_connectivity else cm
