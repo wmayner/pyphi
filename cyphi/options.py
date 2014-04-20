e8fe6e7f6fe4df0e03b3908838576558ecaf73ce
Will Mayner <wmayner@gmail.com>
2014-04-19 20:28:50 -0500

    Add options module

---
+++ cyphi/options.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""User-specified options."""
+
+# TODO! implement this
+# In some applications of this library, the user may prefer to simply consider
+# single-node subsystems as having zero phi.
+CONSIDER_SINGLE_NODES_AS_COMPLEXES = True
