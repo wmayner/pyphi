48277b636e5203f0f057c27fc439a828e202b8e3
Will Mayner <wmayner@gmail.com>
2014-04-21 18:57:15 -0500

    Use Google's string formatting style

--- cyphi/validate.py
+++ cyphi/validate.py
@@ -37,15 +37,15 @@ def direction(direction):
 
 def nodelist(nodes, name):
     if not isinstance(nodes, Iterable):
-        raise ValueError(name + " must be an iterable.")
+        raise ValueError("{} must be an iterable.".format(name))
     if not all(isinstance(node, Node) for node in nodes):
-        raise ValueError(name + " must consist only of Nodes (perhaps you " +
-                         "gave a node's index instead?)")
+        raise ValueError("{} must consist only of Nodes (perhaps you "
+                         "gave a node's index instead?)".format(name))
 
 
 def cut(subsystem, partition):
     severed, intact = partition[0], partition[1]
-    # Convert single nodes to singleton tuples and iterables to tuples
+    # Convert single nodes to singleton tuples and iterables to tuples.
     if not isinstance(severed, Iterable):
         severed = (severed,)
     else:
@@ -54,25 +54,25 @@ def cut(subsystem, partition):
         intact = (intact,)
     else:
         intact = tuple(intact)
-    # Validate
+    # Validate.
     if not (len(subsystem.nodes) == len(severed + intact) and
             set(subsystem.nodes) == set(severed + intact)):
-        raise ValueError("Each node in the subsystem must appear exactly once \
-                         in the partition.")
+        raise ValueError("Each node in the subsystem must appear exactly once "
+                         "in the partition.")
     return Cut(severed, intact)
 
 
 def tpm(tpm):
     if (tpm.shape[-1] != len(tpm.shape) - 1):
-        raise ValueError(
-            """Invalid TPM: There must be a dimension for each node, each one
-            being the size of the corresponding node's state space, plus one
-            dimension that is the same size as the network.""")
+        raise ValueError("Invalid TPM: There must be a dimension for each node, "
+                         "each one being the size of the corresponding node's "
+                         "state space, plus one dimension that is the same size "
+                         "as the network.")
     # TODO extend to nonbinary nodes
     if (tpm.shape[:-1] != tuple([2] * tpm.shape[-1])):
-        raise ValueError(
-            """Invalid TPM: We can only handle binary nodes at the moment. Each
-            dimension except the last must be of size 2.""")
+        raise ValueError("Invalid TPM: We can only handle binary nodes at the "
+                         "moment. Each dimension except the last must be of size "
+                         "2.")
 
 
 # TODO! test
@@ -84,8 +84,8 @@ def connectivity_matrix(network):
         if cm.shape[0] != cm.shape[1]:
             raise ValueError("Connectivity matrix must be square.")
         if cm.shape[0] != network.size:
-            raise ValueError("Connectivity matrix must be NxN, where N is the \
-                             number of nodes in the network.")
+            raise ValueError("Connectivity matrix must be NxN, where N is the "
+                             "number of nodes in the network.")
 
 
 # TODO test
@@ -110,19 +110,20 @@ def state(network):
     """Validate a network's current and past state."""
     current_state, past_state = network.current_state, network.past_state
     tpm = network.tpm
+    # Check that the state is the right size.
     if (len(current_state) != network.size or len(past_state) != network.size):
-        raise ValueError(
-            "Invalid state: there must be one entry per node in the network; " +
-            "this state has " + str(network.current_state.size) + " entries, " +
-            "but there \ are " + str(network.size) + " nodes.")
+        raise ValueError("Invalid state: there must be one entry per node in "
+                         "the network; this state has {} entries, but there "
+                         "are {} nodes.".format(network.current_state.size,
+                                                network.size))
+    # Check that the state is reachable from some state.
     if not _state_reachable(current_state, tpm):
-        raise StateUnreachableError(
-            current_state, past_state, tpm,
+        raise StateUnreachableError(current_state, past_state, tpm,
             "The current state is unreachable according to the given TPM.")
+    # Check that the state is reachable from the given past state.
     if not _state_reachable_from(past_state, current_state, tpm):
-        raise StateUnreachableError(
-            current_state, past_state, tpm,
-            "The current state cannot be reached from the past state " +
+        raise StateUnreachableError(current_state, past_state, tpm,
+            "The current state cannot be reached from the past state "
             "according to the given TPM.")
 
 
