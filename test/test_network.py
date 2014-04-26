a3027b066ccbf889ec245ddf2f19fa936b018e99
Will Mayner <wmayner@gmail.com>
2014-04-25 19:43:04 -0500

    Update network tests to use tuples instead of ndarrays for state

--- test/test_network.py
+++ test/test_network.py
@@ -11,8 +11,8 @@ from cyphi.node import Node
 @pytest.fixture()
 def network():
     size = 3
-    current_state = np.array([0., 1., 0.])
-    past_state = np.array([1, 1, 0])
+    current_state = (0, 1, 0)
+    past_state = (1, 1, 0)
     tpm = np.ones([2] * size + [size]).astype(float) / 2
     return Network(tpm, current_state, past_state)
 
@@ -21,19 +21,19 @@ def test_network_init_validation(network):
     with pytest.raises(ValueError):
         # Totally wrong shape
         tpm = np.arange(3).astype(float)
-        state = np.array([0, 1, 0])
-        past_state = np.array([1, 1, 0])
+        state = (0, 1, 0)
+        past_state = (1, 1, 0)
         Network(tpm, state, past_state)
     with pytest.raises(ValueError):
         # Non-binary nodes (4 states)
         tpm = np.ones((4, 4, 4, 3)).astype(float)
-        state = np.array([0, 1, 0])
+        state = (0, 1, 0)
         Network(tpm, state, past_state)
     with pytest.raises(ValueError):
-        state = np.array([0, 1])
+        state = (0, 1)
         Network(network.tpm, state, network.past_state)
     with pytest.raises(ValueError):
-        state = np.array([0, 1])
+        state = (0, 1)
         Network(network.tpm, network.current_state, state)
     # TODO test state validation (are current and past states congruent to
     # TPM?)
