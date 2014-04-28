d2caaf3897028f5917fc3752e1ca0c453d98a70e
Will Mayner <wmayner@gmail.com>
2014-04-28 15:24:26 -0500

    Test against big network, only mark really slow tests as slow

--- test/test_big_phi.py
+++ test/test_big_phi.py
@@ -4,14 +4,11 @@
 import pytest
 import numpy as np
 
-from cyphi import constants
-from cyphi import utils
-from cyphi.models import Concept
-from cyphi import compute
+import cyphi
 
 
 def test_null_concept(s):
-    assert (s.null_concept() == Concept(
+    assert (s.null_concept() == cyphi.models.Concept(
         mechanism=(),
         location=np.array([s.unconstrained_cause_repertoire(s.nodes),
                            s.unconstrained_effect_repertoire(s.nodes)]),
@@ -27,9 +24,39 @@ def test_concept(s):
     pass
 
 
+def big_phi_standard_example(subsystem):
+    initial_precision = cyphi.constants.PRECISION
+    cyphi.constants.PRECISION = 4
+    assert cyphi.utils.phi_eq(cyphi.compute.big_phi(subsystem), 2.3125)
+    cyphi.constants.PRECISION = initial_precision
+
+
+def test_big_phi_standard_example_sequential(s):
+    initial = cyphi.options.PARALLEL_CUT_EVALUATION
+    cyphi.options.PARALLEL_CUT_EVALUATION = False
+    big_phi_standard_example(s)
+    cyphi.options.PARALLEL_CUT_EVALUATION = initial
+
+
+def test_big_phi_standard_example_parallel(s):
+    initial = cyphi.options.PARALLEL_CUT_EVALUATION
+    cyphi.options.PARALLEL_CUT_EVALUATION = True
+    big_phi_standard_example(s)
+    cyphi.options.PARALLEL_CUT_EVALUATION = initial
+
+
 @pytest.mark.slow
-def test_big_phi_standard_example(s):
-    initial_precision = constants.PRECISION
-    constants.PRECISION = 4
-    assert utils.phi_eq(compute.big_phi(s), 2.3125)
-    constants.PRECISION = initial_precision
+def test_big_mip_big_network(big_subsys_all):
+    initial_precision = cyphi.constants.PRECISION
+    cyphi.constants.PRECISION = 4
+    mip = cyphi.compute.big_mip(big_subsys_all)
+    assert cyphi.utils.phi_eq(mip.phi, 10.744578)
+    assert cyphi.utils.phi_eq(
+        sum(C.phi for C in mip.unpartitioned_constellation),
+        6.464257107140015)
+    assert cyphi.utils.phi_eq(
+        sum(c.phi for c in mip.partitioned_constellation),
+        3.564907)
+    assert len(mip.unpartitioned_constellation) == 30
+    assert len(mip.partitioned_constellation) == 17
+    cyphi.constants.PRECISION = initial_precision
