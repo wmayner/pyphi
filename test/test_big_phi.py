c67dd7ea604f2ca12fd9df11e51d9affffd6e7d7
Will Mayner <wmayner@gmail.com>
2014-05-16 15:42:52 -0500

    Update big_phi tests

--- test/test_big_phi.py
+++ test/test_big_phi.py
@@ -4,8 +4,7 @@
 import pytest
 import numpy as np
 
-import cyphi
-from cyphi import options
+from cyphi import options, compute, models, utils
 
 from . import example_networks as examples
 
@@ -16,11 +15,12 @@ PRECISION = 4
 STANDARD_EXAMPLE_PHI = 2.3125
 # Compute the BigMip of the standard example full subsystem, once
 # Don't interact with cache
-big_mip_standard_example = cyphi.compute._big_mip.func(examples.s())
+big_mip_standard_example = compute.big_mip.func(examples.s(),
+                                                      hash(examples.s()))
 
 
 def test_null_concept(s):
-    assert (s.null_concept() == cyphi.models.Concept(
+    assert (s.null_concept() == models.Concept(
         mechanism=(),
         location=np.array([s.unconstrained_cause_repertoire(s.nodes),
                            s.unconstrained_effect_repertoire(s.nodes)]),
@@ -28,13 +28,13 @@ def test_null_concept(s):
 
 
 def test_concept_nonexistent(s):
-    assert not s.concept((s.nodes[0], s.nodes[2]))
+    assert not compute.concept(s, models.Mechanism((s.nodes[0], s.nodes[2])))
 
 
 def test_big_phi_standard_example_sequential(s):
     initial = options.PARALLEL_CUT_EVALUATION
     options.PARALLEL_CUT_EVALUATION = False
-    phi = cyphi.compute._big_mip.func(s).phi
+    phi = compute.big_mip.func(s, hash(s)).phi
     np.testing.assert_almost_equal(phi, STANDARD_EXAMPLE_PHI, PRECISION)
     options.PARALLEL_CUT_EVALUATION = initial
 
@@ -42,7 +42,7 @@ def test_big_phi_standard_example_sequential(s):
 def test_big_phi_standard_example_parallel(s):
     initial = options.PARALLEL_CUT_EVALUATION
     options.PARALLEL_CUT_EVALUATION = True
-    phi = cyphi.compute._big_mip.func(s).phi
+    phi = compute.big_mip.func(s, hash(s)).phi
     np.testing.assert_almost_equal(phi, STANDARD_EXAMPLE_PHI, PRECISION)
     options.PARALLEL_CUT_EVALUATION = initial
 
@@ -61,15 +61,15 @@ def test_standard_example(s):
 
 
 @pytest.mark.slow
-def test_big_mip_big_network(big_subsys_all):
+def testbig_mip_big_network(big_subsys_all):
     initial_precision = options.PRECISION
     options.PRECISION = 4
-    mip = cyphi.compute.big_mip(big_subsys_all)
-    assert cyphi.utils.phi_eq(mip.phi, 10.744578)
-    assert cyphi.utils.phi_eq(
+    mip = compute.big_mip(big_subsys_all)
+    assert utils.phi_eq(mip.phi, 10.744578)
+    assert utils.phi_eq(
         sum(C.phi for C in mip.unpartitioned_constellation),
         6.464257107140015)
-    assert cyphi.utils.phi_eq(
+    assert utils.phi_eq(
         sum(c.phi for c in mip.partitioned_constellation),
         3.564907)
     assert len(mip.unpartitioned_constellation) == 30
