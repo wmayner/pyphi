71655b01461351052615deaeddb077f74c9e114a
Will Mayner <wmayner@gmail.com>
2014-06-04 15:36:30 -0500

    Update requirements for new marbl-python

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
     'pyemd >=0.0.7, <1.0.0',
     'joblib >=0.8.0a3, <1.0.0',
     'psutil >= 2.1.1, <3.0.0',
-    'marbl-python >=0.0.2, <1.0.0'
+    'marbl-python >=1.0.0, <2.0.0'
 ]
 
 test_require = [
