BUG: Fix for concept normalization problem
This fix is (functional) garbage. It's being pushed now so long
simulations can be started, and will be refactored ASAP.

cyphi/network.py
@@ -67,7 +67,7 @@ class Network:
     """
 
     def __init__(self, tpm, current_state, past_state,
-                 connectivity_matrix=None):
+                 connectivity_matrix=None, boundary_indices=None):
         # Cast TPM to np.array.
         tpm = np.array(tpm)
         # Convert to state-by-node if we were given a square state-by-state
@@ -116,8 +116,9 @@ class Network:
         validate.network(self)
 
         # Generate the nodes.
-        self.nodes = tuple([Node(self, node_index) for node_index in
-                            range(self.size)])
+        self.nodes = tuple([Node(self, node_index,
+            boundary_indices=boundary_indices) for node_index in
+            range(self.size)])
 
         # TODO extend to nonbinary nodes
         self.num_states = 2 ** self.size

cyphi/node.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np
+from itertools import chain
 import functools
+import numpy as np
 from marbl import Marbl
 from . import utils
 
@@ -10,7 +11,7 @@ from . import utils
 # TODO extend to nonbinary nodes
 # TODO? refactor to use purely indexes for nodes
 @functools.total_ordering
-class Node(object):
+class Node:
 
     """A node in a network.
 
@@ -35,33 +36,78 @@ class Node(object):
         at |t_{-1}|.
     """
 
-    def __init__(self, network, index, label=None):
+    def __init__(self, network, index, label=None, boundary_indices=None):
+        self.context = 'network'
+        if boundary_indices is not None:
+            self.context = 'subsystem'
+        else:
+            boundary_indices = ()
         # This node's parent network.
         self.network = network
         # This node's index in the network's list of nodes.
         self.index = index
         # Label for display.
         self.label = label
+        # Indices of nodes to be considered as boundary conditions. This node's
+        # TPM will be conditioned on the state of these nodes.
+        self.boundary_indices = boundary_indices
 
         # Get indices of the inputs.
         self._input_indices = np.array(
             [i for i in range(self.network.size) if
-                self.network.connectivity_matrix[i][self.index]])
+                self.network.connectivity_matrix[i][self.index] and
+                i not in self.boundary_indices])
         self._output_indices = np.array(
             [i for i in range(self.network.size) if
-                self.network.connectivity_matrix[self.index][i]])
+                self.network.connectivity_matrix[self.index][i] and
+                i not in self.boundary_indices])
 
+        # Generate the node's TPM.
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Get the part of the network's TPM that gives just the state of this
+        # node. This part is still indexed by network state, but its last
+        # dimension will be gone, since now there's just a single scalar value
+        # (this node's state) rather than a state-vector for all the network
+        # nodes.
+        tpm_on = self.network.tpm[..., self.index]
+        # Now we condition on the fixed states of the boundary nodes, if any.
+        for node_index in self.boundary_indices:
+            # First we initialize the conditioning indices, taking the slices
+            # as singleton lists-of-lists for later flattening with `chain`.
+            conditioning_indices = [[slice(None)]] * self.network.size
+            # Preserve singleton dimensions with `np.newaxis`.
+            conditioning_indices[node_index] = \
+                [self.network.current_state[node_index], np.newaxis]
+            # Flatten the indices.
+            conditioning_indices = list(
+                chain.from_iterable(conditioning_indices))
+            # Obtain the conditioned TPM by indexing with the conditioning
+            # indices; this collapses the TPM onto the fixed state of the
+            # boundary node, making that node's corresponding dimension into a
+            # singleton.
+            conditioned_part = tpm_on[conditioning_indices]
+            # Now we expand the boundary node's dimension to the size of the
+            # boundary node's state-space again, but this time all the entries
+            # correspond to that nodes fixed state. This allows us to index
+            # into the TPM normally, as if the boundary node could have any
+            # state, but the results of calculations will reflect that it is in
+            # one fixed state.
+            tpm_on = np.concatenate((conditioned_part, conditioned_part),
+                                    node_index)
+        # Get the TPM that gives the probability of the node being off, rather
+        # than on.
+        tpm_off = 1 - tpm_on
+
+        # Marginalize-out non-input nodes. Note that this does not include
+        # fixed nodes that may be inputs according to the connectivity matrix;
+        # these have already been dealt with.
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # This will hold the indices of the nodes that correspond to
         # non-singleton dimensions of this node's on-TPM. It maps any network
         # node index to the corresponding dimension of this node's TPM with
         # singleton dimensions removed. We need this for creating this node's
         # Marbl.
         self._dimension_labels = []
-        # Generate the node's TPM.
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        tpm_on = self.network.tpm[..., self.index]
-        tpm_off = 1 - tpm_on
-        # Marginalize-out non-input nodes.
         current_non_singleton_dim_index = 0
         for index in range(self.network.size):
             if index not in self._input_indices:
@@ -78,10 +124,12 @@ class Node(object):
                 # for the next one.
                 self._dimension_labels.append(current_non_singleton_dim_index)
                 current_non_singleton_dim_index += 1
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
         # Store the generated TPM.
         self.tpm = np.array([tpm_off, tpm_on])
-        # Make it immutable (for hashing).
+
+        # Make the TPM immutable (for hashing).
         self.tpm.flags.writeable = False
 
         # Only compute hash once.

cyphi/subsystem.py
@@ -16,6 +16,7 @@ from .constants import DIRECTIONS, PAST, FUTURE
 from .lru_cache import lru_cache
 from . import constants, validate, utils
 from .models import Cut, Mip, Part, Mice, Concept
+from .node import Node
 
 
 # TODO! go through docs and make sure to say when things can be None
@@ -32,18 +33,24 @@ class Subsystem:
     """
 
     def __init__(self, node_indices, current_state, past_state, network):
-        # This nodes in this subsystem.
-        # (Remove duplicates and sort)
-        self.nodes = tuple(sorted(list(set(network.nodes[i] for i in
-                                           node_indices))))
-        self.node_indices = utils.nodes2indices(self.nodes)
-        self.size = len(self.node_indices)
-
-        self.current_state = current_state
-        self.past_state = past_state
-
         # The network this subsystem belongs to.
         self.network = network
+        # Remove duplicates and sort node indices.
+        node_indices = tuple(sorted(list(set(node_indices))))
+        self.node_indices = node_indices
+        self.size = len(self.node_indices)
+        # Get the external nodes.
+        self.external_indices = tuple(
+            set(range(network.size)) - set(self.node_indices))
+        # Generate the nodes.
+        self.nodes = tuple(
+            Node(self.network, i, boundary_indices=self.external_indices) for i
+            in self.node_indices)
+
+        # TODO subsystems don't need states, user should just make a new
+        # network.
+        self.current_state = current_state
+        self.past_state = past_state
 
         # The null cut (leaves the system intact).
         self.null_cut = Cut(severed=(), intact=self.nodes)
@@ -99,6 +106,9 @@ class Subsystem:
     def __hash__(self):
         return self._hash
 
+    def indices2nodes(self, indices):
+        return tuple(n for n in self.nodes if n.index in indices)
+
     @lru_cache(maxmem=constants.MAXIMUM_CACHE_MEMORY_PERCENTAGE)
     def cause_repertoire(self, mechanism, purview, cut=None):
         """Return the cause repertoire of a mechanism over a purview.
@@ -129,6 +139,7 @@ class Subsystem:
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         mechanism = validate.nodelist(mechanism, 'Mechanism')
         purview = validate.nodelist(purview, 'Purview')
+        purview_indices = utils.nodes2indices(purview)
         # If the mechanism is empty, nothing is specified about the past state
         # of the purview, so just return the purview's maximum entropy
         # distribution.
@@ -144,8 +155,8 @@ class Subsystem:
             cut = self.null_cut
         # Preallocate the mechanism's conditional joint distribution.
         # TODO extend to nonbinary nodes
-        cjd = np.ones(tuple(2 if node in purview else
-                            1 for node in self.network.nodes))
+        cjd = np.ones(tuple(2 if i in purview_indices else
+                            1 for i in range(self.network.size)))
         # Loop over all nodes in this mechanism, successively taking the
         # product (with expansion/broadcasting of singleton dimensions) of each
         # individual node's TPM (conditioned on that node's state) in order to
@@ -162,14 +173,17 @@ class Subsystem:
             conditioned_tpm = mechanism_node.tpm[node_state]
             # Collect the nodes that are not in the purview and have
             # connections to this node.
-            non_purview_inputs = (inputs &
-                                  (set(self.network.nodes) - set(purview)))
-            # Collect the nodes in the network who had inputs to this mechanism
-            # that were severed by this subsystem's cut.
+            non_purview_inputs = inputs & (set(self.network.nodes) - set(purview))
+            # Collect the nodes in the subsystem who had inputs to this
+            # mechanism that were severed by this subsystem's cut.
             severed_inputs = (inputs &
-                              set([n for n in self.network.nodes if
+                              set([n for n in self.nodes if
                                    (n in cut.severed and mechanism_node in
                                     cut.intact)]))
+            # # We will marginalize-out nodes that are within the subsystem, but
+            # # are either not in the purview or severed by a cut.
+            # marginal_inputs = (non_purview_inputs | severed_inputs)
+
             # Fixed boundary-condition nodes are those that are outside this
             # subsystem, and are either not in the purview or have been severed
             # by a cut.
@@ -188,6 +202,7 @@ class Subsystem:
                 conditioning_indices[node.index] = \
                     [self.past_state[node.index]]
                 conditioned_tpm = conditioned_tpm[conditioning_indices]
+
             # Marginalize-out the nodes in this subsystem with inputs to this
             # mechanism that are either not in the purview or whose connections
             # to this mechanism have not been severed by a subsystem cut.
@@ -245,6 +260,7 @@ class Subsystem:
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         mechanism = validate.nodelist(mechanism, 'Mechanism')
         purview = validate.nodelist(purview, 'Purview')
+        purview_indices = utils.nodes2indices(purview)
         # If the purview is empty, the distribution is empty, so return the
         # multiplicative identity.
         if not purview:
@@ -255,8 +271,8 @@ class Subsystem:
         # Preallocate the purview's joint distribution
         # TODO extend to nonbinary nodes
         accumulated_cjd = np.ones(
-            [1] * self.network.size + [2 if node in purview else 1 for node in
-                                       self.network.nodes])
+            [1] * self.network.size + [2 if i in purview_indices else 1 for i in
+                                       range(self.network.size)])
         # Loop over all nodes in the purview, successively taking the product
         # (with 'expansion'/'broadcasting' of singleton dimensions) of each
         # individual node's TPM in order to get the joint distribution for the
@@ -278,7 +294,8 @@ class Subsystem:
 
             inputs = set(purview_node.inputs)
             # TODO extend to nonbinary nodes
-            # Rotate the dimensions so the first dimension is the last
+            # Rotate the dimensions so the first dimension is the last (the
+            # first dimension corresponds to the state of the node)
             tpm = purview_node.tpm
             tpm = tpm.transpose(list(range(tpm.ndim))[1:] + [0])
             # Expand the dimensions so the TPM can be indexed as described
@@ -289,9 +306,9 @@ class Subsystem:
 
             # Collect nodes whose connections to this purview were severed by
             # the cut.
-            severed_nodes = set([n for n in self.network.nodes if (
+            severed_nodes = set([n for n in self.nodes if (
                 n in cut.severed and purview_node in cut.intact)])
-            # Collect the nodes in the network who had connections to this
+            # Collect the nodes in the subsystem who had connections to this
             # purview node that were severed by this subsystem's cut.
             severed_mechanism_nodes = severed_nodes & set(mechanism)
             # We marginalize-out inputs to the current purview node that are
@@ -346,7 +363,7 @@ class Subsystem:
         # (the second half of the shape may also contain singleton dimensions,
         # depending on how many nodes are in the purview).
         accumulated_cjd = accumulated_cjd.reshape(
-            accumulated_cjd.shape[self.network.size:2 * self.network.size])
+            accumulated_cjd.shape[self.network.size:(2 * self.network.size)])
 
         # Note that we're not returning a distribution over all the nodes in
         # the network, only a distribution over the nodes in the purview. This
@@ -768,6 +785,7 @@ class Subsystem:
 
     def concept(self, mechanism, cut=None):
         """Calculate a concept."""
+        # TODO refactor to pass indices around, not nodes, throughout Subsystem
         # Calculate the maximally irreducible cause repertoire.
         cause = self.core_cause(mechanism, cut)
         # Calculate the maximally irreducible effect repertoire.