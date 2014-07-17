365816f05c628afb65bf80065434bfc43f801ce2
Will Mayner <wmayner@gmail.com>
2014-07-17 16:30:17 -0500

    Update concept_caching for new Subystems/Concepts
    - Nodes now have two TPMs, one for past and one for future, so
      NormalizedMechanisms need two marblsets, etc.
    - The context of a Concept is now a Subsystem, not a Network
    - Concepts now have a subsystem attribute

--- cyphi/concept_caching.py
+++ cyphi/concept_caching.py
@@ -14,7 +14,6 @@ import numpy as np
 from marbl import MarblSet
 
 from . import utils, models, db
-from .network import Network
 from .constants import DIRECTIONS, PAST, FUTURE
 
 
@@ -52,6 +51,9 @@ class NormalizedMechanism:
             normalized index.
         outputs (tuple(tuple(int))): The same as ``inputs``, but the elements
             contain normalized indices of the outputs.
+        permutation (dict()): A dictionary where the keys are directions and
+            the values are the permutations that maps mechanism nodes to the
+            position of their marbl in the marblset for that direction.
     """
 
     # NOTE: We use lists and indices throughout, instead of dictionaries (which
@@ -60,28 +62,39 @@ class NormalizedMechanism:
     def __init__(self, mechanism, subsystem, normalize_tpms=True):
         self.indices = utils.nodes2indices(mechanism)
         # Grab the marbls from the mechanism nodes.
-        marbls = [(n.marbl if normalize_tpms else n.raw_marbl) for n in
-                  mechanism]
-        self.marblset = MarblSet(marbls)
-        M = range(len(self.marblset))
+        marbls = {
+            DIRECTIONS[PAST]: [(n.past_marbl if normalize_tpms else
+                                n.raw_past_marbl) for n in mechanism],
+            DIRECTIONS[FUTURE]: [(n.current_marbl if normalize_tpms else
+                                  n.raw_current_marbl) for n in mechanism]
+        }
+        self.marblset = {
+            direction: MarblSet(marbls[direction]) for direction in DIRECTIONS
+        }
+        self.permutation = {
+            direction: self.marblset[direction].permutation
+            for direction in DIRECTIONS
+        }
+        M = range(len(mechanism))
         # Associate marbls in the marblset to the mechanism nodes they were
         # generated from.
-        marbl_preimage = [
-            # The ith marbl corresponds to the jth node in the mechanism, where
-            # j is the image of i under the marblset's permutation.
-            mechanism[self.marblset.permutation[i]]
-            for i, marbl in enumerate(self.marblset)
-        ]
+        marbl_preimage = {
+            direction: [
+                # The ith marbl corresponds to the jth node in the mechanism,
+                # where j is the image of i under the marblset's permutation.
+                mechanism[self.permutation[direction][i]]
+                for i, marbl in enumerate(self.marblset[direction])
+            ] for direction in DIRECTIONS
+        }
         # Associate each marbl to the inputs of its preimage node.
         io = {
             # Inputs
             DIRECTIONS[PAST]: [
-                marbl_preimage[m].inputs for m in M],
+                marbl_preimage[DIRECTIONS[PAST]][m].inputs for m in M],
             # Outputs
             DIRECTIONS[FUTURE]: [
-                marbl_preimage[m].outputs for m in M]
+                marbl_preimage[DIRECTIONS[FUTURE]][m].outputs for m in M]
         }
-
         # Now, we generate the normalized index of each node that inputs
         # (outputs) to at least one mechanism node. Also record the reverse
         # mapping, that sends normalized indices to the original indices.
@@ -102,9 +115,6 @@ class NormalizedMechanism:
             DIRECTIONS[PAST]: {},
             DIRECTIONS[FUTURE]: {}
         }
-        # This will hold the normalized indices of the inputs/outputs that lie
-        # outside the subsystem.
-        external_normalized_indices = set()
         # This will be used to label newly-encountered inputs/outpus.
         counter = {DIRECTIONS[PAST]: 0, DIRECTIONS[FUTURE]: 0}
         for d in DIRECTIONS:
@@ -116,9 +126,6 @@ class NormalizedMechanism:
                         normal_index = counter[d]
                         # Assign the next unused integer as the label.
                         self.normalized_indices[d][node.index] = normal_index
-                        # Record if the input/putput is external.
-                        if node.index not in subsystem.node_indices:
-                            external_normalized_indices.add(normal_index)
                         # Increment the counter so the next label is different.
                         counter[d] += 1
         # Get the inverse mappings.
@@ -142,22 +149,12 @@ class NormalizedMechanism:
             tuple(self.normalized_indices[DIRECTIONS[FUTURE]][n.index]
                   for n in io[DIRECTIONS[FUTURE]][m])
             for m in M)
-        # Convert the set of external normalized indices to a tuple (for
-        # hashing).
-        self.external_indices = tuple(external_normalized_indices)
-
-    @property
-    def permutation(self):
-        """
-        ``tuple(int)`` -- The permutation that maps mechanism nodes to the
-        position of their marbl in the marblset.
-        """
-        return self.marblset.permutation
 
     # TODO!!!: make hash independent of python
     def __hash__(self):
-        return hash((self.marblset, self.inputs, self.outputs,
-                     self.external_indices))
+        return hash((self.marblset[DIRECTIONS[PAST]],
+                     self.marblset[DIRECTIONS[FUTURE]],
+                     self.inputs, self.outputs))
 
     def __str__(self):
         return str(self.indices)
@@ -243,7 +240,7 @@ class NormalizedConcept:
 
     Upon initialization, the normal form of the concept to be cached is
     computed, and data relating its cause and effect purviews are stored,
-    which the concept to be reconstituted in a different network.
+    which the concept to be reconstituted in a different subsystem.
 
     Attributes:
         mechanism (NormalizedMechanism): The mechanism the concept consists of.
@@ -275,12 +272,13 @@ class NormalizedConcept:
 def _unnormalize_purview_and_repertoire(normalized_purview,
                                         normalized_repertoire,
                                         unnormalized_indices,
-                                        network):
+                                        subsystem):
+    network = subsystem.network
     # Get the unnormalized purview indices.
     purview_indices = tuple(unnormalized_indices[normal_index]
                             for normal_index in normalized_purview)
     # Get the actual purview nodes.
-    purview = network.indices2nodes(purview_indices)
+    purview = subsystem.indices2nodes(purview_indices)
     # If the normalized repertoire is None, from a null MIP, return
     # immediately.
     if normalized_repertoire is None:
@@ -302,14 +300,14 @@ def _unnormalize_purview_and_repertoire(normalized_purview,
     return purview, repertoire
 
 
-def _unnormalize_mice(normalized_mice, normalized_mechanism, network):
+def _unnormalize_mice(normalized_mice, normalized_mechanism, subsystem):
     """Convert a normalized MICE to its proper representation in the context of
-    a network.
+    a subsystem.
 
     .. warning::
         Information about the underlying MIP's partition is lost during
         normalization since it is dependent on the specific structure of the
-        network in which the MIP was computed. Thus, the underlying MIPs of
+        subsystem in which the MIP was computed. Thus, the underlying MIPs of
         unnormalized MICE have no parition or partitioned repertoire.
     """
     # Get the unnormalized purview and repertoire.
@@ -317,7 +315,7 @@ def _unnormalize_mice(normalized_mice, normalized_mechanism, network):
         normalized_mice.purview,
         normalized_mice.repertoire,
         normalized_mechanism.unnormalized_indices[normalized_mice.direction],
-        network)
+        subsystem)
     return models.Mice(models.Mip(
         phi=normalized_mice.phi,
         direction=normalized_mice.direction,
@@ -330,20 +328,22 @@ def _unnormalize_mice(normalized_mice, normalized_mechanism, network):
     ))
 
 
-def _unnormalize(normalized_concept, normalized_mechanism, mechanism, network):
+def _unnormalize(normalized_concept, normalized_mechanism, mechanism,
+                 subsystem):
     """Convert a normalized concept to its proper representation in the context
-    of the given network."""
+    of the given subsystem."""
     cause = _unnormalize_mice(normalized_concept.cause,
                               normalized_mechanism,
-                              network)
+                              subsystem)
     effect = _unnormalize_mice(normalized_concept.effect,
                                normalized_mechanism,
-                               network)
+                               subsystem)
     return models.Concept(
         phi=normalized_concept.phi,
         mechanism=mechanism,
         cause=cause,
-        effect=effect)
+        effect=effect,
+        subsystem=subsystem)
 
 
 def _get(raw, normalized_mechanism, mechanism, subsystem):
@@ -354,7 +354,7 @@ def _get(raw, normalized_mechanism, mechanism, subsystem):
     if normalized_concept is None:
         return None
     concept = _unnormalize(normalized_concept, normalized_mechanism, mechanism,
-                           subsystem.network)
+                           subsystem)
     return concept
 
 
@@ -365,15 +365,13 @@ def _set(normalized_mechanism, concept):
     return db.insert(key, value)
 
 
-def concept(subsystem, mechanism, cut=None):
+def concept(subsystem, mechanism):
     """Find the concept specified by a mechanism, returning a cached value if
     one is found and computing and caching it otherwise."""
-    if cut is None:
-        cut = subsystem.null_cut
     # First we try to retrieve the concept without normalizing TPMs, which is
     # expensive.
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    raw_normalized_mechanism = NormalizedMechanism(mechanism, cut, subsystem,
+    raw_normalized_mechanism = NormalizedMechanism(mechanism, subsystem,
                                                    normalize_tpms=False)
     # See if we have a precomputed value without normalization.
     cached_concept = _get(True, raw_normalized_mechanism, mechanism, subsystem)
@@ -382,7 +380,7 @@ def concept(subsystem, mechanism, cut=None):
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # We didn't find a precomputed concept with the raw normalized TPM, so now
     # we normalize TPMs as well.
-    normalized_mechanism = NormalizedMechanism(mechanism, cut, subsystem)
+    normalized_mechanism = NormalizedMechanism(mechanism, subsystem)
     # Try to retrieve the concept with the fully-normalized mechanism.
     cached_concept = _get(False, normalized_mechanism, mechanism, subsystem)
     if cached_concept is not None:
@@ -391,7 +389,7 @@ def concept(subsystem, mechanism, cut=None):
     # We didn't find any precomputed concept at all, so compute it, and store
     # the result with the raw normalized mechanism and the fully-normalized
     # mechanism as keys.
-    concept = subsystem.concept(mechanism, cut)
+    concept = subsystem.concept(mechanism)
     _set(raw_normalized_mechanism, concept)
     _set(normalized_mechanism, concept)
     return concept
