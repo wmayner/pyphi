83b976770b07d580344fc7415f1e7a6219943fbe
Will Mayner <wmayner@gmail.com>
2014-08-11 14:41:07 -0500

    Add builtin logging to __init__

--- cyphi/__init__.py
+++ cyphi/__init__.py
@@ -43,3 +43,60 @@ __copyright__ = 'Copyright 2014 Will Mayner'
 from .network import Network
 from .subsystem import Subsystem
 from . import compute, constants, db, examples
+
+import os
+import yaml
+import logging.config
+
+
+# Default user-configured logging options.
+logging_config = {
+    'format': "%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+    # `level` can be "DEBUG", "INFO", "WARNING", "ERROR", or "CRITICAL".
+    'file': {
+        'enabled': True,
+        'level': "INFO",
+        'filename': "cyphi.log"
+    },
+    'stdout': {
+        'enabled': True,
+        'level': "INFO",
+    }
+}
+
+
+# Load user's logging configuration if available.
+if os.path.exists(constants.CYPHI_CONFIG_FILE):
+    with open(constants.CYPHI_CONFIG_FILE) as f:
+        config = yaml.load(f)
+        logging_config = config['LOGGING_CONFIG']
+
+
+# Configure logging module.
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': logging_config['format']
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': logging_config['file']['level'],
+            'class': 'logging.FileHandler',
+            'filename': logging_config['file']['filename'],
+            'formatter': 'standard',
+        },
+        'stdout': {
+            'level': logging_config['stdout']['level'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': [h for h in ['file', 'stdout'] if
+                     logging_config[h]['enabled']]
+    }
+})
