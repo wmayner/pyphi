d74f4b840e95241efc46a769f55b149bb53fadc4
Will Mayner <wmayner@gmail.com>
2014-10-15 22:43:29 -0500

    Bump version

--- pyphi/__init__.py
+++ pyphi/__init__.py
@@ -42,7 +42,7 @@ options and their defaults.
 """
 
 __title__ = 'pyphi'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __description__ = 'Python library for computing integrated information.',
 __author__ = 'Will Mayner'
 __author_email__ = 'wmayner@gmail.com'
