c519e47335bbc56f065254b6608de8388ee9adbb
Will Mayner <wmayner@gmail.com>
2014-11-05 18:06:13 -0600

    Forgot to make it a Mice

--- pyphi/subsystem.py
+++ pyphi/subsystem.py
@@ -635,7 +635,7 @@ class Subsystem:
         purviews = tuple(filter(not_trivially_reducible, purviews))
         # If no purviews are left, return a null MICE immediately.
         if not purviews:
-            return self.find_mip(direction, mechanism, ())
+            return Mice(self.find_mip(direction, mechanism, ()))
         # Find the maximal MIP over all purviews.
         maximal_mip = max(self.find_mip(direction, mechanism, purview) for
                           purview in purviews)
