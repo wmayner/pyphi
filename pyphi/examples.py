7e899b3d65d003b266dd1c588a37c3896a88841a
William Marshall <wmarshall3@wisc.edu>
2014-12-16 10:46:19 -0600

    Adding function to create residue example subsystem

--- pyphi/examples.py
+++ pyphi/examples.py
@@ -170,3 +170,157 @@ def subsystem():
     :func:`pyphi.examples.network`."""
     net = network()
     return Subsystem(range(net.size), net)
+
+
+"""
+Residue Example
+
+-------------------------------------------------------------------------------
+
+The residue example describes  a system containing two AND nodes A and B
+with a single overlapping input node
+
+First lets create the subsystem corresponding to the residue network
+
+   >>> subsystem = residue_subsystem()
+
+Next we can define the mechanisms of interest
+
+   >>> A = subsystem.indices2nodes((0,))
+   >>> B = subsystem.indices2nodes((1,))
+   >>> AB = subsystem.indices2nodes((0,1))
+
+and possible past purviews
+
+   >>> CD = subsystem.indices2nodes((2,3))
+   >>> DE = subsystem.indices2nodes((3,4))
+   >>> CDE = subsystem.indices2nodeS((2,3,4))
+
+We can then evaluate the cause information for each of the mechanisms over the
+past purview CDE,
+
+   >>> subsystem.cause_info(A, CDE)
+   0.33333191666400036
+
+   >>> subsystem.cause_info(B, CDE)
+   0.33333191666400036
+
+   >>> subsystem.cause_info(AB, CDE)
+   0.49999972500000006
+
+The composite mechanism AB has greater cause information than either of the individual
+mechanisms. This goes against the idea that AB should exist minimally in this system.
+
+Instead, we can quantify existence as the irreducible cause information of a mechanism.
+The MIP of a mechansim is the partition of mechansim and purview which makes the least
+difference to the cause repertoire. The irreducible cause information is the distance
+between the unpartitioned and partitioned repertoires.
+
+To calculate the MIP structure of AB
+
+   >>> mip_AB = subsystem.mip_past(AB, CDE)
+
+We can then determine what the specific partition is
+
+   >>> mip_AB.partition
+   (Part(mechanism=(), purview=(n2,)), Part(mechanism=(n0, n1), purview=(n3, n4)))
+
+The labels (n0, n1, n2, n3, n4) correspond to nodes (A,B,C,D,E) repsectively. Thus the
+MIP is (AB | DE) X ([] | C), where [] denotes the empty mechanism.
+
+The partitioned repertoire of the MIP can also be retrieved,
+
+   >>> mip_AB.partitioned_repertoire.reshape([8], order='F')
+   array([ 0.2,  0.2,  0.1,  0.1,  0.2,  0.2,  0. ,  0. ])
+
+and we can then calculate the irreducible cause information as the difference between
+paritioned and unpartitioned repertoires,
+
+   >>> mip_AB.phi
+   0.09999990000000035
+
+One counter-intuitive result which merrits discussion is that since irreducible cause information
+is what defines existence, we must also evaluate the irreducible cause information of
+the mechansims A and B.
+
+The mechanism A over the purview CDE is completely reducible to (A|CD) x ([]|E) because E has no
+effect on A, there is no output returned from the subsystem.mip_past function,
+
+   >>> subsystem.mip_past(A, CDE)
+
+Instead, we should evaluate A over the purview CD,
+
+   >>> mip_A = subsystem.mip_past(A, CD)
+
+In this case, there is a well defined MIP
+
+   >>> mip_A.partition
+   (Part(mechanism=(), purview=(n2,)), Part(mechanism=(n0,), purview=(n3,)))
+
+which is ([]|C) x (A|D). It has partitioned repertoire
+
+   >>> mip_A.partitioned_repertoire.reshape([4], order='F')
+   array([ 0.33333333,  0.33333333,  0.16666667,  0.16666667])
+
+and irreducible cause information
+
+   >>> mip_A.phi
+   0.16666700000000023
+
+A similar result holds for B. Thus the mechansims A and B exist at levels of phi=1/6, while
+the higher-order mechanism AB exists only as the residual of causes, at a level of phi=1/10.
+
+"""
+
+
+def residue_subsystem():
+    """ The subsystem of the residue network containing all nodes. Current and past state
+    are all off (0,0,0,0,0)
+
+    Diagram:
+
+            +~~~~~~~~+           +~~~~~~~~+
+        +~~>|   A    |<~~+  +~~~>|   B    |<~~~+
+        |   | (AND)  |   |  |    | (AND)  |    |
+        |   +~~~~~~~~+   |  |    +~~~~~~~~+    |
+        |                |  |                  |
+        |                |  |                  |
+        |                |  |                  |
+        |                |  |                  |
+    +~~~~~~~~+        +~~~~~~~~+        +~~~~~~~~+
+    |   C    |        |   D    |        |   E    |
+    |        |        |        |        |        |
+    +~~~~~~~~+        +~~~~~~~~+        +~~~~~~~~+
+
+    Connectivity matrix:
+    +---+---+---+---+---+---+
+    | . | A | B | C | D | E |
+    +---+---+---+---+---+---+
+    | A | 0 | 0 | 0 | 0 | 0 |
+    +---+---+---+---+---+---+
+    | B | 0 | 0 | 0 | 0 | 0 |
+    +---+---+---+---+---+---+
+    | C | 1 | 0 | 0 | 0 | 0 |
+    +---+---+---+---+---+---+
+    | D | 1 | 1 | 0 | 0 | 0 |
+    +---+---+---+---+---+---+
+    | E | 0 | 1 | 0 | 0 | 0 |
+    +---+---+---+---+---+---+
+
+    """
+
+    tpm = np.array([[int(s) for s in bin(x)[2:].zfill(5)[::-1]] for x in range(32)])
+    tpm[np.where(np.sum(tpm[0:,2:4],1)==2),0] = 1
+    tpm[np.where(np.sum(tpm[0:,3:5],1)==2),1] = 1
+    tpm[np.where(np.sum(tpm[0:,2:4],1)<2),0] = 0
+    tpm[np.where(np.sum(tpm[0:,3:5],1)<2),1] = 0
+
+    cm = np.zeros((5,5))
+    cm[2:4, 0] = 1
+    cm[3:, 1] = 1
+
+    current_state = (0,0,0,0,0)
+    past_state = (0,0,0,0,0)
+
+    network = Network(tpm, current_state, past_state, connectivity_matrix=cm)
+    return Subsystem(range(network.size), network)
