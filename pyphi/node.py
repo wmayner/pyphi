1b206152343742c146c40283169c98ae0a378be4
Will Mayner <wmayner@gmail.com>
2015-06-18 17:21:08 -0500

    Update Node docs and comments to reflect past state removal

--- pyphi/node.py
+++ pyphi/node.py
@@ -29,26 +29,6 @@ class Node:
             The subsystem the node belongs to.
         label (str):
             An optional label for the node.
-        inputs (list(Node)):
-            A list of nodes that have connections to this node.
-        past_tpm (np.ndarray):
-            The TPM for this node, conditioned on the past state of the
-            boundary nodes, whose states are fixed. ``this_node.past_tpm[0]``
-            and ``this_node.past_tpm[1]`` gives the probability tables that
-            this node is off and on, respectively, indexed by subsystem state,
-            **after marginalizing-out nodes that don't connect to this node**.
-        current_tpm (np.ndarray):
-            Same as ``past_tpm``, but conditioned on the current state of the
-            boundary nodes.
-
-    Examples:
-        In a 3-node subsystem, ``self.past_tpm[0][(0, 0, 1)]`` gives the
-        probability that this node is off at |t_0| if the state of the network
-        is |N_0 = 0, N_1 = 0, N_2 = 1| at |t_{-1}|.
-
-        Similarly, ``self.current_tpm[1][(0, 0, 1)]`` gives the probability
-        that this node is on at |t_1| if the state of the network is |N_0 = 0,
-        N_1 = 0, N_2 = 1| at |t_0|.
     """
 
     def __init__(self, network, index, subsystem, label=None):
@@ -67,17 +47,19 @@ class Node:
             self.index, subsystem.connectivity_matrix)
         self._output_indices = utils.get_outputs_from_cm(
             self.index, subsystem.connectivity_matrix)
-        # Generate the node's TPMs.
+
+        # Generate the node's TPM.
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # For the past and current state, get the part of the subsystem's TPM
-        # that gives just the state of this node. This part is still indexed by
-        # network state, but its last dimension will be gone, since now there's
-        # just a single scalar value (this node's state) rather than a
-        # state-vector for all the network nodes.
+        # We begin by getting the part of the subsystem's TPM that gives just
+        # the state of this node. This part is still indexed by network state,
+        # but its last dimension will be gone, since now there's just a single
+        # scalar value (this node's state) rather than a state-vector for all
+        # the network nodes.
         tpm_on = self.subsystem.tpm[..., self.index]
-        # Get the TPMs that give the probability of the node being off, rather
+        # Get the TPM that gives the probability of the node being off, rather
         # than on.
         tpm_off = 1 - tpm_on
+
         # Marginalize-out non-input subsystem nodes and get dimension labels.
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # This list will hold the indices of the nodes that correspond to
@@ -91,7 +73,6 @@ class Node:
         # Iterate over all the nodes in the network, since we need to keep
         # track of all singleton dimensions.
         for i in range(self.network.size):
-
             # Input nodes that are within the subsystem will correspond to a
             # dimension in this node's squeezed TPM, so we map it to the index
             # of the corresponding dimension and increment the corresponding
@@ -105,16 +86,16 @@ class Node:
             # don't give them a dimension label.
             else:
                 self._dimension_labels.append(None)
-
             # TODO extend to nonbinary nodes
             # Marginalize out non-input nodes that are in the subsystem, since
             # the external nodes have already been dealt with as boundary
-            # conditions in the subsystem's TPMs.
-            if i not in self._input_indices and i in self.subsystem.node_indices:
+            # conditions in the subsystem's TPM.
+            if (i not in self._input_indices
+                    and i in self.subsystem.node_indices):
                 tpm_on = tpm_on.sum(i, keepdims=True) / 2
                 tpm_off = tpm_off.sum(i, keepdims=True) / 2
 
-        # Combine the on- and off-TPMs.
+        # Combine the on- and off-TPM.
         self.tpm = np.array([tpm_off, tpm_on])
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -137,10 +118,9 @@ class Node:
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def get_marbl(self, normalize=True):
-        """Generate a Marbl for this node, using either the past or current
-        TPM."""
-        # We take only the part of the TPM giving the probability the node
-        # is on.
+        """Generate a Marbl for this node TPM."""
+        # We take only the part of the TPM giving the probability the node is
+        # on.
         # TODO extend to nonbinary nodes
         augmented_child_tpms = [
             [child._dimension_labels[self.index],
