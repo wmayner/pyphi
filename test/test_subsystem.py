8bb1614abf68854cb896c0fae09df246919edcec
Will Mayner <wmayner@gmail.com>
2015-06-19 17:12:33 -0500

    Update test_subsystem to refactoring and add validation test

--- test/test_subsystem.py
+++ test/test_subsystem.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# test_subsystem.py
+
+import pytest
+import example_networks
 
 from pyphi.subsystem import Subsystem
 from pyphi.models import Cut
 import numpy as np
 
-def test_empty_init(standard):
+
+def test_subsystem_validation(s):
+    # Wrong state length.
+    with pytest.raises(ValueError):
+        s = Subsystem(s.network, [0, 0], s.node_indices)
+    # Wrong state values.
+    with pytest.raises(ValueError):
+        s = Subsystem(s.network, [2, 0, 0], s.node_indices)
+    # Unreachable state.
+    with pytest.raises(ValueError):
+        net = example_networks.simple()
+        s = Subsystem(net, [1, 1, 1], s.node_indices)
+
+
+def test_empty_init(s):
     # Empty mechanism
-    s = Subsystem((), standard)
+    s = Subsystem(s.network, s.state, ())
     assert s.nodes == ()
 
 
@@ -36,19 +54,23 @@ def test_len(s, big_subsys_0_thru_3, big_subsys_all):
 def test_hash(s):
     print(hash(s))
 
+
 def test_find_cut_matrix(s, big_subsys_0_thru_3):
-    cut = Cut((0,), (1,2))
-    cut_s = Subsystem(s.node_indices,
-                      s.network,
-                      cut=cut,
-                      mice_cache=s._mice_cache)
+    cut = Cut((0, ), (1, 2))
+    cut_s = Subsystem(
+        s.network, s.state, s.node_indices, cut=cut, mice_cache=s._mice_cache)
     answer_s = np.array([[0, 1, 1], [0, 0, 0], [0, 0, 0]])
-    cut = Cut((0,1), (2,3))
-    cut_big = Subsystem(big_subsys_0_thru_3.node_indices,
-                        big_subsys_0_thru_3.network,
+    cut = Cut((0, 1), (2, 3))
+    cut_big = Subsystem(big_subsys_0_thru_3.network,
+                        big_subsys_0_thru_3.state,
+                        big_subsys_0_thru_3.node_indices,
                         cut=cut,
                         mice_cache=big_subsys_0_thru_3._mice_cache)
-    answer_big = np.array([[0, 0, 1, 1], [0, 0, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]])
+    answer_big = np.array([
+        [0, 0, 1, 1],
+        [0, 0, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ])
     assert np.array_equal(cut_s.cut_matrix, answer_s)
     assert np.array_equal(cut_big.cut_matrix, answer_big)
-
