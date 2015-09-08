f432ecebf9d9555b074c653b35ede065c23876bf
Will Mayner <wmayner@gmail.com>
2015-09-08 16:16:47 -0500

    Update JSON tests to check native types

--- test/test_json.py
+++ test/test_json.py
@@ -6,12 +6,22 @@
 import pyphi
 
 
-def test_json_big_mip(s, flushcache, restore_fs_cache):
+def test_jsonify_native(s):
+    x = {
+        'list': [1, 2.0, 3],
+        'tuple': (1, 2, 3),
+        'bool': [True, False],
+        'null': None
+    }
+    pyphi.jsonify.loads(pyphi.jsonify.dumps(x))
+
+
+def test_jsonify_big_mip(s, flushcache, restore_fs_cache):
     flushcache()
     pyphi.jsonify.loads(pyphi.jsonify.dumps(pyphi.compute.big_mip(s)))
 
 
-def test_json_complexes(s, flushcache, restore_fs_cache):
+def test_jsonify_complexes(s, flushcache, restore_fs_cache):
     flushcache()
     complexes = pyphi.compute.complexes(s.network, s.state)
     pyphi.jsonify.loads(pyphi.jsonify.dumps(complexes))
