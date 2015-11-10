9fb60ffe54bc64a4734806e5aa08765583007608
Will Mayner <wmayner@gmail.com>
2015-11-09 20:24:05 -0600

    Default to validating subsystem states in absence of config file

--- pyphi/config.py
+++ pyphi/config.py
@@ -300,7 +300,7 @@ DEFAULTS = {
     'PRECISION': 6,
     # Controls whether a subsystem's state is validated when the subsystem is
     # created.
-    'VALIDATE_SUBSYSTEM_STATES': False,
+    'VALIDATE_SUBSYSTEM_STATES': True,
     # In some applications of this library, the user may prefer to define
     # single-node subsystems as having 0.5 Phi.
     'SINGLE_NODES_WITH_SELFLOOPS_HAVE_PHI': False,
