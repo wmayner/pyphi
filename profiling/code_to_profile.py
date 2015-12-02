afbcb454c4491f869bdb3969bf5389d2ed1ef55c
Bo Marchman <bo.marchman@gmail.com>
2015-12-02 13:29:59 -0600

    Fix profiling scripts for new Network

--- profiling/code_to_profile.py
+++ profiling/code_to_profile.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+After running this code, either
+- Use `python -m pstats [file.pstats]` for an interactive pstats prompt.
+- Use the `makecallgraph` script to visualize the call graph.
+"""
+
 import logging
 import os
 import sys
@@ -18,15 +24,17 @@ from joblib import Parallel, delayed
 formatter = logging.Formatter(
     fmt='%(asctime)s [%(name)s] %(levelname)s: %(message)s')
 
+PSTATS = 'pstats'
+LOGS = 'logs'
+RESULTS = 'results'
+
 
 def json2pyphi(network):
     tpm = network['tpm']
     current_state = network['currentState']
-    past_state = network['pastState']
     cm = network['connectivityMatrix']
-    network = pyphi.Network(tpm, current_state, past_state,
-                            connectivity_matrix=cm)
-    return network
+    network = pyphi.Network(tpm, connectivity_matrix=cm)
+    return (network, current_state)
 
 
 network_types = [
@@ -44,7 +52,7 @@ for n in network_sizes:
 
 def profile_network(filename):
     log = logging.getLogger(filename)
-    handler = logging.FileHandler('logs/' + filename + '.log')
+    handler = logging.FileHandler(LOGS + '/' + filename + '.log')
     handler.setFormatter(formatter)
     log.addHandler(handler)
     log.setLevel(logging.INFO)
@@ -52,7 +60,7 @@ def profile_network(filename):
     try:
         with open('networks/' + filename + '.json') as f:
 
-            net = json2pyphi(json.load(f))
+            (network, state) = json2pyphi(json.load(f))
 
             print(''.center(72, '-'))
             log.info('Profiling {}...'.format(filename))
@@ -63,19 +71,31 @@ def profile_network(filename):
             pr = cProfile.Profile()
             pr.enable()
 
-            results = tuple(pyphi.compute.complexes(net))
+            results = tuple(pyphi.compute.complexes(network, state))
 
             pr.disable()
             end = time()
 
-            pr.dump_stats('pstats/' + filename + '.pstats')
+            pr.dump_stats(PSTATS + '/' + filename + '.pstats')
 
             log.info('Finished in {} seconds.'.format(end - start))
-            with open('pyphi_results/' + filename + '-results.pkl', 'wb') as f:
+            with open(RESULTS + '/' + filename + '-results.pkl', 'wb') as f:
                 pickle.dump(results, f)
     except Exception as e:
         log.error(e)
         pass
 
-Parallel(n_jobs=(-5), verbose=20)(
-    delayed(profile_network)(filename) for filename in network_files)
+
+def ensure_dir(dirname):
+    """Make a directory if it does not already exist"""
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+
+
+if __name__ == "__main__":
+    ensure_dir(PSTATS)
+    ensure_dir(LOGS)
+    ensure_dir(RESULTS)
+
+    Parallel(n_jobs=(-5), verbose=20)(
+        delayed(profile_network)(filename) for filename in network_files)
