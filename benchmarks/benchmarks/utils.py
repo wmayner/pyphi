4be28f9d089a6cb93c32d5f1d8b88bb0f58d4678
Bo Marchman <bo.marchman@gmail.com>
2015-12-27 15:17:36 -0500

    Add reducibility benchmark

--- benchmarks/benchmarks/utils.py
+++ benchmarks/benchmarks/utils.py
@@ -3,14 +3,14 @@ import numpy as np
 from pyphi import utils
 
 
-cm1 = [
+cm0 = [
     [1, 1, 0, 0, 0],
     [1, 1, 0, 0, 0],
     [0, 0, 0, 1, 0],
     [0, 0, 1, 1, 1],
     [1, 0, 0, 1, 1]]
 
-cm2 = [
+cm1 = [
     [0, 1, 1, 0, 1, 0, 0],
     [1, 0, 1, 0, 0, 0, 0],
     [1, 1, 0, 0, 0, 0, 0],
@@ -19,7 +19,7 @@ cm2 = [
     [0, 0, 0, 0, 0, 1, 1],
     [0, 0, 0, 0, 0, 1, 1]]
 
-cm3 = [
+cm2 = [
     [1, 1, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 0, 0, 0, 0],
@@ -29,10 +29,10 @@ cm3 = [
     [0, 0, 0, 0, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 1, 1]]
 
-cm4 = [[1] * 16] * 16
+cm3 = [[1] * 16] * 16
 
 
-matrices = [cm1, cm2, cm3, cm4]
+matrices = [cm0, cm1, cm2, cm3]
 
 
 class BenchmarkBlockCm():
@@ -44,3 +44,21 @@ class BenchmarkBlockCm():
 
     def time_block_cm(self, m):
         utils.block_cm(self.cm)
+
+
+class BenchmarkReducibility():
+
+    params = [[0, 1, 2, 3], ['block', 'full']]
+    param_names = ['cm', 'type']
+
+    def setup(self, m, _type):
+        self.cm = np.array(matrices[m])
+
+    def time_reducibility(self, m, _type):
+        idxs = (tuple(range(self.cm.shape[0])),
+                tuple(range(self.cm.shape[1])))
+
+        if _type == 'block':
+            utils.block_reducible(self.cm, *idxs)
+        elif _type == 'full':
+            utils.fully_connected(self.cm, *idxs)
