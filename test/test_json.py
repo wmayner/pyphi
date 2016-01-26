2a1da0da777c2ca18d3084d705d882446f26b41c
Bo Marchman <bo.marchman@gmail.com>
2016-01-25 22:35:22 -0500

    Add some `jsonify` tests

--- test/test_json.py
+++ test/test_json.py
@@ -2,21 +2,44 @@
 # -*- coding: utf-8 -*-
 # test_json.py
 
+import numpy as np
 
 import pyphi
 
 
-# TODO these tests need to be fleshed out, they don't do much
-
-
-def test_jsonify_native(s):
+def test_jsonify_native():
     x = {
         'list': [1, 2.0, 3],
         'tuple': (1, 2, 3),
         'bool': [True, False],
         'null': None
     }
-    pyphi.jsonify.loads(pyphi.jsonify.dumps(x))
+    answer = {
+        'list': [1, 2.0, 3],
+        'tuple': [1, 2, 3],
+        'bool': [True, False],
+        'null': None
+    }
+    assert answer == pyphi.jsonify.loads(pyphi.jsonify.dumps(x))
+
+
+def test_jsonify_numpy():
+    x = {
+        'ndarray': np.array([1, 2]),
+        'np.int32': np.int32(1),
+        'np.int64': np.int64(2),
+        'np.float64': np.float64(3),
+    }
+    answer = {
+        'ndarray': [1, 2],
+        'np.int32': 1,
+        'np.int64': 2,
+        'np.float64': 3.0,
+    }
+    assert answer == pyphi.jsonify.loads(pyphi.jsonify.dumps(x))
+
+
+# TODO: these tests need to be fleshed out, they don't do much
 
 
 def test_jsonify_big_mip(s, flushcache, restore_fs_cache):
