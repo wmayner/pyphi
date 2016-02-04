wmayner/pyphi

commit e65461a9c5217f34e745fa082b370fed98b55b1e

Document loading config from a file

pyphi/config.py
@@ -12,6 +12,15 @@ The various options are listed here with their defaults
     >>> import pyphi
     >>> defaults = pyphi.config.DEFAULTS
 
+It is also possible to manually load a YAML configuration file within your
+script:
+
+    >>> pyphi.config.load_config_file('pyphi_config.yml')
+
+Or load a dictionary of configuration values:
+
+    >>> pyphi.config.load_config({'PRECISION': 7})
+
 
 Theoretical approximations
 ~~~~~~~~~~~~~~~~~~~~~~~~~~

commit 1177c65020e152e965508226c65c40df8b37e311

Fix bug introduced by e65461a
The docstring example of setting `PRECISION` changed the value for
all test cases, leading to rounding errors in many tests.

pyphi/config.py
@@ -19,7 +19,7 @@ script:
 
 Or load a dictionary of configuration values:
 
-    >>> pyphi.config.load_config_dict({'PRECISION': 7})
+    >>> pyphi.config.load_config_dict({'SOME_CONFIG': 'value'})
 
 
 Theoretical approximations