ce6df45343f421866d28b3d6cb668b2d02fa782f
Bo Marchman <bo.marchman@gmail.com>
2016-02-08 13:35:52 -0500

    Remove erroneous comma

--- pyphi/__about__.py
+++ pyphi/__about__.py
@@ -6,7 +6,7 @@
 
 __title__ = 'pyphi'
 __version__ = '0.8.1'
-__description__ = 'Python library for computing integrated information.',
+__description__ = 'Python library for computing integrated information.'
 __author__ = 'Will Mayner'
 __author_email__ = 'wmayner@gmail.com'
 __author_website__ = 'http://willmayner.com'
