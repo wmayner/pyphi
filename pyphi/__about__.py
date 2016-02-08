b2f0a99e68fce86a3a0e9faff202acd27549e43c
Bo Marchman <bo.marchman@gmail.com>
2016-02-08 13:51:48 -0500

    Bump copyright year

--- pyphi/__about__.py
+++ pyphi/__about__.py
@@ -10,7 +10,7 @@ __description__ = 'Python library for computing integrated information.'
 __author__ = 'Will Mayner'
 __author_email__ = 'wmayner@gmail.com'
 __author_website__ = 'http://willmayner.com'
-__copyright__ = 'Copyright 2014-2015 Will Mayner'
+__copyright__ = 'Copyright 2014-2016 Will Mayner'
 __license__ = 'GNU General Public License v3.0'
 
 __all__ = ['__title__', '__version__', '__description__', '__author__',
