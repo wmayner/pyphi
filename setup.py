715e80d9b4ffa5d91e04870a885c109e8dab74e4
Will Mayner <wmayner@gmail.com>
2016-02-08 14:17:37 -0600

    Add Python 3.5 classifier to `setup.py`

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering',
     ]
 )
