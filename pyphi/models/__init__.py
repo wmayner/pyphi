5c67c6f29d8fc0216383626f44871126323357ec
Bo Marchman <bo.marchman@gmail.com>
2016-02-12 18:42:16 -0500

    Document `models.__init__`

--- pyphi/models/__init__.py
+++ pyphi/models/__init__.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 # models/__init__.py
 
+"""See |models.big_phi|, |models.concept|, and |models.cuts| for documentation.
+
+Attributes:
+    BigMip: Alias for :class:`big_phi.BigMip`
+    Mip: Alias for :class:`concept.Mip`
+    Mice: Alias for :class:`concept.Mice`
+    Concept: Alias for :class:`concept.Concept`
+    Constellation: Alias for :class:`concept.Constellation`
+    Cut: Alias for :class:`cuts.Cut`
+    Part: Alias for :class:`cuts.Part`
+"""
+
 from .big_phi import BigMip, _null_bigmip, _single_node_bigmip
 from .concept import Mip, _null_mip, Mice, Concept, Constellation
 from .cuts import Cut, Part
