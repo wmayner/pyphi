f5349748688e9673ecc60f8ae7963b632697a8db
Bo Marchman <bo.marchman@gmail.com>
2016-02-24 13:55:18 -0500

    Move tpms inside relevant test functions

--- test/test_macro_subsystem.py
+++ test/test_macro_subsystem.py
@@ -76,66 +76,67 @@ def test_macro_cut_subsystem(macro_subsystem):
 # Tests for purely temporal blackboxing
 # =====================================
 
-tpm_noise = np.array([
-    [0.25, 0.25, 0.25, 0.25],
-    [0.25, 0.25, 0.25, 0.25],
-    [0.25, 0.25, 0.25, 0.25],
-    [0.25, 0.25, 0.25, 0.25]
-])
-
-tpm_copy = sbn2sbs(np.array([
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [0, 1, 1],
-    [1, 0, 0],
-    [1, 1, 0],
-    [1, 0, 1],
-    [1, 1, 1]
-]))
-
-tpm_copy2 = sbn2sbs(np.array([
-    [0, 0, 0],
-    [0, 0, 1],
-    [1, 0, 0],
-    [1, 0, 1],
-    [0, 1, 0],
-    [0, 1, 1],
-    [1, 1, 0],
-    [1, 1, 1]
-]))
-
-tpm_copy3 = sbn2sbs(np.array([
-    [0, 0, 0],
-    [1, 0, 0],
-    [0, 1, 0],
-    [1, 1, 0],
-    [0, 0, 1],
-    [1, 0, 1],
-    [0, 1, 1],
-    [1, 1, 1]
-]))
-
-tpm_huge = np.array([[1 if i == j+1 else 0
-                      for i in range(1000)]
-                     for j in range(1000)])
-tpm_huge[999, 0] = 1
 
 
 def test_sparse_blackbox():
+    tpm_huge = np.array([[1 if i == j+1 else 0
+                          for i in range(1000)]
+                         for j in range(1000)])
+    tpm_huge[999, 0] = 1
     assert np.array_equal(utils.sparse_time(tpm_huge, 1001), tpm_huge)
 
 
 def test_dense_blackbox():
+    tpm_noise = np.array([
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25],
+        [0.25, 0.25, 0.25, 0.25]
+    ])
     assert np.array_equal(utils.dense_time(tpm_noise, 2), tpm_noise)
     assert np.array_equal(utils.dense_time(tpm_noise, 3), tpm_noise)
 
 
 def test_cycle_blackbox():
-    assert np.array_equal(utils.sparse_time(tpm_copy, 2), tpm_copy2)
-    assert np.array_equal(utils.sparse_time(tpm_copy, 3), tpm_copy3)
-    assert np.array_equal(utils.dense_time(tpm_copy, 2), tpm_copy2)
-    assert np.array_equal(utils.dense_time(tpm_copy, 3), tpm_copy3)
+    tpm = sbn2sbs(np.array([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [0, 1, 1],
+        [1, 0, 0],
+        [1, 1, 0],
+        [1, 0, 1],
+        [1, 1, 1]
+    ]))
+
+    # tpm over 2 timesteps
+    tpm2 = sbn2sbs(np.array([
+        [0, 0, 0],
+        [0, 0, 1],
+        [1, 0, 0],
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 1, 0],
+        [1, 1, 1]
+    ]))
+
+    # tpm over 3 timesteps
+    tpm3 = sbn2sbs(np.array([
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 0, 1],
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1]
+    ]))
+
+    assert np.array_equal(utils.sparse_time(tpm, 2), tpm2)
+    assert np.array_equal(utils.sparse_time(tpm, 3), tpm3)
+    assert np.array_equal(utils.dense_time(tpm, 2), tpm2)
+    assert np.array_equal(utils.dense_time(tpm, 3), tpm3)
 
 
 def test_run_tpm():
