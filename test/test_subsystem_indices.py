9798ee5251136749e0924e63dda10a076253abd6
Bo Marchman <bo.marchman@gmail.com>
2016-02-24 13:37:28 -0500

    Rewrite macro subsystem tests
    Test actual generated TPMs instead of various index attributes.
    This hides implementation details and should make it easier to
    simplify `MacroSubsystem` initialization.

--- test/test_subsystem_indices.py
+++ test/test_subsystem_indices.py
@@ -2,178 +2,83 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import pyphi
-from pyphi import macro
-
-pyphi.config.CACHE_POTENTIAL_PURVIEWS = False
-
-Nodes = 6
-
-tpm = np.zeros((2 ** Nodes, Nodes))
-state = (0, 0, 0, 0, 0, 0)
-network = pyphi.Network(tpm)
+from pyphi import macro, Network
 
 
-# No external, blackbox, or coarse grain
-subsystem1 = macro.MacroSubsystem(network, state, network.node_indices)
-
-# External elements
-subsystem2 = macro.MacroSubsystem(network, state, (0, 2, 4, 5))
-
-# Hidden elements, no External
-hidden_indices = (3, 4)
-subsystem3 = macro.MacroSubsystem(network, state, network.node_indices,
-                                  hidden_indices=hidden_indices)
-
-# Hidden and External elements
-hidden_indices = (1, 3)
-subsystem4 = macro.MacroSubsystem(network, state, (0, 1, 3, 5),
-                                  hidden_indices=hidden_indices)
-
-# Only macro
-output_grouping = ((0, 1, 2), (3, 4, 5))
-state_grouping = (((1,), (0, 2, 3)), ((0,), (1, 2, 3)))
-coarse_grain = macro.CoarseGrain(output_grouping, state_grouping)
-subsystem5 = macro.MacroSubsystem(network, state, network.node_indices,
-                                  coarse_grain=coarse_grain)
-# Macro and External
-output_grouping = ((1, 3), (2, 5))
-state_grouping = (((0, 1), (2,)), ((1,), (0, 2)))
-coarse_grain = macro.CoarseGrain(output_grouping, state_grouping)
-subsystem6 = macro.MacroSubsystem(network, state, (1, 2, 3, 5),
-                                  coarse_grain=coarse_grain)
-
-# Macro and Hidden
-hidden_indices = (3, 4)
-output_grouping = ((0, 5), (1, 2))
-state_grouping = (((1,), (0, 2)), ((0,), (1, 2)))
-coarse_grain = macro.CoarseGrain(output_grouping, state_grouping)
-subsystem7 = macro.MacroSubsystem(network, state, network.node_indices,
-                                  hidden_indices=hidden_indices,
-                                  coarse_grain=coarse_grain)
-
-# External, Macro and Hidden
-hidden_indices = (4,)
-output_grouping = ((1,), (2,), (3, 5))
-state_grouping = (((0,), (1,)), ((1,), (0,)), ((0,), (1, 2)))
-coarse_grain = macro.CoarseGrain(output_grouping, state_grouping)
-subsystem8 = macro.MacroSubsystem(network, state, (1, 2, 3, 4, 5),
-                                  hidden_indices=hidden_indices,
-                                  coarse_grain=coarse_grain)
+def test_blackbox(s):
+    ms = macro.MacroSubsystem(s.network, s.state, s.node_indices,
+                              hidden_indices=(0, 2))
+    # Conditioned on hidden indices and squeezed
+    assert np.array_equal(ms.tpm, np.array([[0], [0]]))
+    # Universal connectivity
+    assert np.array_equal(ms.cm, np.array([[1]]))
+    # Reindexed
+    assert ms.node_indices == (0,)
+    assert ms.state == (0,)
+    assert not ms.is_micro
 
 
-pyphi.config.CACHE_POTENTIAL_PURVIEWS = True
+def test_blackbox_external(s):
+    # Which is the same if one of these indices is external
+    ms = macro.MacroSubsystem(s.network, s.state, (1, 2), hidden_indices=(2,))
+    assert np.array_equal(ms.tpm, np.array([[0], [0]]))
+    assert np.array_equal(ms.cm, np.array([[1]]))
+    assert ms.node_indices == (0,)
+    assert ms.state == (0,)
 
 
-def test_size():
-    assert subsystem1.size == 6
-    assert subsystem2.size == 4
-    assert subsystem3.size == 4
-    assert subsystem4.size == 2
-    assert subsystem5.size == 2
-    assert subsystem6.size == 2
-    assert subsystem7.size == 2
-    assert subsystem8.size == 3
+def test_coarse_grain(s):
+    coarse_grain = macro.CoarseGrain(output_grouping=((0, 1), (2,)),
+                                     state_grouping=((((0, 1), (2,)), ((0,), (1,)))))
+    ms = macro.MacroSubsystem(s.network, s.state, s.node_indices,
+                              coarse_grain=coarse_grain)
+    answer_tpm = np.array(
+        [[[0, 0.66666667],
+          [1, 0.66666667]],
+         [[0, 0],
+          [1, 0]]])
+    assert np.allclose(ms.tpm, answer_tpm)
+    assert np.array_equal(ms.cm, np.ones((2, 2)))
+    assert ms.node_indices == (0, 1)
+    assert ms.state == (0, 0)
 
 
-def test_external_indices():
-    assert subsystem1.external_indices == ()
-    assert subsystem2.external_indices == (1, 3)
-    assert subsystem3.external_indices == ()
-    assert subsystem4.external_indices == (2, 4)
-    assert subsystem5.external_indices == ()
-    assert subsystem6.external_indices == (0, 4)
-    assert subsystem7.external_indices == ()
-    assert subsystem8.external_indices == (0,)
+def test_blackbox_and_coarse_grain(s):
+    coarse_grain = macro.CoarseGrain(output_grouping=((0, 2),),
+                                     state_grouping=((((0, 1), (2,)),)))
+    ms = macro.MacroSubsystem(s.network, s.state, s.node_indices,
+                              hidden_indices=(1,), coarse_grain=coarse_grain)
+    assert np.array_equal(ms.tpm, np.array([[0], [1]]))
+    assert np.array_equal(ms.cm, [[1]])
+    assert ms.node_indices == (0,)
+    assert ms.size == 1
+    assert ms.state == (0,)
 
 
-def test_internal_indices():
-    assert subsystem1.internal_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem2.internal_indices == (0, 2, 4, 5)
-    assert subsystem3.internal_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem4.internal_indices == (0, 1, 3, 5)
-    assert subsystem5.internal_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem6.internal_indices == (1, 2, 3, 5)
-    assert subsystem7.internal_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem8.internal_indices == (1, 2, 3, 4, 5)
+def test_blackbox_and_coarse_grain_external(s):
+    # Larger, with external nodes, blackboxed and coarse-grained
+    tpm = np.zeros((2 ** 6, 6))
+    network = Network(tpm)
+    state = (0, 0, 0, 0, 0, 0)
 
-
-def test_micro_indices():
-    assert subsystem1.micro_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem2.micro_indices == (0, 1, 2, 3)
-    assert subsystem3.micro_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem4.micro_indices == (0, 1, 2, 3)
-    assert subsystem5.micro_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem6.micro_indices == (0, 1, 2, 3)
-    assert subsystem7.micro_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem8.micro_indices == (0, 1, 2, 3, 4)
-
-
-def test_hidden_indices():
-    assert subsystem1.hidden_indices == ()
-    assert subsystem2.hidden_indices == ()
-    assert subsystem3.hidden_indices == (3, 4)
-    assert subsystem4.hidden_indices == (1, 2)
-    assert subsystem5.hidden_indices == ()
-    assert subsystem6.hidden_indices == ()
-    assert subsystem7.hidden_indices == (3, 4)
-    assert subsystem8.hidden_indices == (3,)
-
-
-def test_output_indices():
-    assert subsystem1.output_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem2.output_indices == (0, 1, 2, 3)
-    assert subsystem3.output_indices == (0, 1, 2, 5)
-    assert subsystem4.output_indices == (0, 3)
-    assert subsystem5.output_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem6.output_indices == (0, 1, 2, 3)
-    assert subsystem7.output_indices == (0, 1, 2, 5)
-    assert subsystem8.output_indices == (0, 1, 2, 4)
-
-
-def test_grouping_indices():
-    assert subsystem1.coarse_grain.output_grouping == ()
-    assert subsystem2.coarse_grain.output_grouping == ()
-    assert subsystem3.coarse_grain.output_grouping == ()
-    assert subsystem4.coarse_grain.output_grouping == ()
-    assert subsystem5.coarse_grain.output_grouping == ((0, 1, 2), (3, 4, 5))
-    assert subsystem6.coarse_grain.output_grouping == ((0, 2), (1, 3))
-    assert subsystem7.coarse_grain.output_grouping == ((0, 3), (1, 2))
-    assert subsystem8.coarse_grain.output_grouping == ((0,), (1,), (2, 3))
-
-
-def test_node_indices():
-    assert subsystem1.node_indices == (0, 1, 2, 3, 4, 5)
-    assert subsystem2.node_indices == (0, 1, 2, 3)
-    assert subsystem3.node_indices == (0, 1, 2, 3)
-    assert subsystem4.node_indices == (0, 1)
-    assert subsystem5.node_indices == (0, 1)
-    assert subsystem6.node_indices == (0, 1)
-    assert subsystem7.node_indices == (0, 1)
-    assert subsystem8.node_indices == (0, 1, 2)
-
-
-def test_subsystem_micro():
-    assert subsystem1.is_micro
-    assert subsystem2.is_micro
-    assert not subsystem3.is_micro
-    assert not subsystem4.is_micro
-    assert not subsystem5.is_micro
-    assert not subsystem6.is_micro
-    assert not subsystem7.is_micro
-    assert not subsystem8.is_micro
-
-
-def test_subsystem_cm():
-    assert np.array_equal(subsystem3.connectivity_matrix, np.ones((4, 4)))
-
-
-def test_state():
-    assert subsystem1.state == (0, 0, 0, 0, 0, 0)
-    assert subsystem2.state == (0, 0, 0, 0)
-    assert subsystem3.state == (0, 0, 0, 0)
-    assert subsystem4.state == (0, 0)
-    assert subsystem5.state == (1, 0)
-    assert subsystem6.state == (0, 1)
-    assert subsystem7.state == (1, 0)
-    assert subsystem8.state == (0, 1, 0)
+    hidden_indices = (4,)
+    output_grouping = ((1,), (2,), (3, 5))
+    state_grouping = (((0,), (1,)), ((1,), (0,)), ((0,), (1, 2)))
+    coarse_grain = macro.CoarseGrain(output_grouping, state_grouping)
+    ms = macro.MacroSubsystem(network, state, (1, 2, 3, 4, 5),
+                              hidden_indices=hidden_indices,
+                              coarse_grain=coarse_grain)
+    answer_tpm = np.array(
+        [[[[0, 1, 0],
+           [0, 1, 0]],
+          [[0, 1, 0],
+           [0, 1, 0]]],
+         [[[0, 1, 0],
+           [0, 1, 0]],
+          [[0, 1, 0],
+           [0, 1, 0]]]])
+    assert np.array_equal(ms.tpm, answer_tpm)
+    assert np.array_equal(ms.cm, np.ones((3, 3)))
+    assert ms.node_indices == (0, 1, 2)
+    assert ms.size == 3
+    assert ms.state == (0, 1, 0)
