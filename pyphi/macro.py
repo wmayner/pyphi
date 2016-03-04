fd2236e9df1a9273c4a17e7a3e5215ac148a7f89
Bo Marchman <bo.marchman@gmail.com>
2016-03-03 21:47:49 -0500

    Validate conditional independence by default

--- pyphi/macro.py
+++ pyphi/macro.py
@@ -296,7 +296,7 @@ class CoarseGrain(namedtuple('CoarseGrain', ['partition', 'grouping'])):
                    for micro_state in micro_states]
         return np.array(mapping)
 
-    def macro_tpm(self, micro_tpm, check_independence=False):
+    def macro_tpm(self, micro_tpm, check_independence=True):
         """Create a coarse-grained macro TPM.
 
         Args:
