25952332f9849fa27b50dbb13e87f6e888bb6f7b
Bo Marchman <bo.marchman@gmail.com>
2016-06-08 15:27:08 -0500

    Ignore `_themes/README.rst`

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '_themes/README.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
