7f0f60e47dd6d0813bd1db51ab0257add9680627
Bo Marchman <bo.marchman@gmail.com>
2016-08-17 23:59:55 -0400

    Whitespace

--- pyphi/jsonify.py
+++ pyphi/jsonify.py
@@ -78,34 +78,36 @@ def jsonify(obj):
     any available ``to_json`` methods, converting NumPy arrays and datatypes to
     native lists and types along the way."""
 
+    # Call the `to_json` method if available and add metadata.
     if hasattr(obj, 'to_json'):
-        # Call the `to_json` method if available.
         d = obj.to_json()
-
-        # Add metadata
         d[CLASS_KEY] = obj.__class__.__name__
         d[VERSION_KEY] = pyphi.__version__
         d[ID_KEY] = id(obj)  # To properly reconstruct the object graph
-
         return jsonify(d)
 
     # If we have a numpy array, convert it to a list.
     if isinstance(obj, np.ndarray):
         return obj.tolist()
+
     # If we have NumPy datatypes, convert them to native types.
     if isinstance(obj, (np.int32, np.int64)):
         return int(obj)
     if isinstance(obj, np.float64):
         return float(obj)
+
     # Recurse over dictionaries.
     if isinstance(obj, dict):
         return _jsonify_dict(obj)
+
     # Recurse over object dictionaries.
     if hasattr(obj, '__dict__'):
         return _jsonify_dict(obj.__dict__)
+
     # Recurse over lists and tuples.
     if isinstance(obj, (list, tuple)):
         return [jsonify(item) for item in obj]
+
     # Otherwise, give up and hope it's serializable.
     return obj
 
