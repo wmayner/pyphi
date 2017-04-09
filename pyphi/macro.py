85310b42a3882808e46e5c7a557069efeba3b588
Bo Marchman <bo.marchman@gmail.com>
2017-04-09 12:30:54 -0400

    Reflow line

--- pyphi/macro.py
+++ pyphi/macro.py
@@ -398,8 +398,7 @@ class MacroSubsystem(Subsystem):
     def _potential_purviews(self, direction, mechanism, purviews=False):
         """Override Subsystem implementation using Network-level indices."""
         all_purviews = utils.powerset(self.node_indices)
-        return irreducible_purviews(self.cm, direction,
-                                    mechanism, all_purviews)
+        return irreducible_purviews(self.cm, direction, mechanism, all_purviews)
 
     def macro2micro(self, macro_indices):
         """Returns all micro indices which compose the elements specified by
