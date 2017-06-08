f9f3992dc5424248dbc0005da3e1cba5c10532fa
Will Mayner <wmayner@gmail.com>
2017-06-08 17:40:51 -0500

    Add `partitions` and `k_partitions` tests

--- test/test_partition.py
+++ test/test_partition.py
@@ -2,7 +2,34 @@
 # -*- coding: utf-8 -*-
 # test/test_partition.py
 
-from pyphi.partition import directed_bipartition, directed_tripartition_indices
+import itertools
+
+from pyphi.partition import (
+    directed_bipartition, directed_tripartition_indices, k_partitions,
+    partitions
+)
+
+
+def test_partitions():
+    assert list(partitions([])) == []
+    assert list(partitions([0])) == [[[0]]]
+    assert list(partitions(range(4))) == [
+        [[0, 1, 2, 3]],
+        [[0], [1, 2, 3]],
+        [[0, 1], [2, 3]],
+        [[1], [0, 2, 3]],
+        [[0], [1], [2, 3]],
+        [[0, 1, 2], [3]],
+        [[1, 2], [0, 3]],
+        [[0], [1, 2], [3]],
+        [[0, 2], [1, 3]],
+        [[2], [0, 1, 3]],
+        [[0], [2], [1, 3]],
+        [[0, 1], [2], [3]],
+        [[1], [0, 2], [3]],
+        [[1], [2], [0, 3]],
+        [[0], [1], [2], [3]]
+    ]
 
 
 def test_directed_bipartition():
@@ -25,3 +52,37 @@ def test_directed_tripartition_indices():
         ((1,), (), (0,)),
         ((), (1,), (0,)),
         ((), (), (0, 1))]
+
+
+def test_k_partition():
+    # Special/edge cases
+    for n, k in list(itertools.product(range(-1, 2), repeat=2))[:-1]:
+        assert list(k_partitions(range(n), k)) == []
+    # Large case
+    assert list(k_partitions(range(5), 3)) == [
+        [[0, 1, 2], [3], [4]],
+        [[0, 1], [2, 3], [4]],
+        [[0], [1, 2, 3], [4]],
+        [[0, 2], [1, 3], [4]],
+        [[0, 2, 3], [1], [4]],
+        [[0, 3], [1, 2], [4]],
+        [[0, 1, 3], [2], [4]],
+        [[0, 1], [2], [3, 4]],
+        [[0], [1, 2], [3, 4]],
+        [[0, 2], [1], [3, 4]],
+        [[0], [1], [2, 3, 4]],
+        [[0], [1, 3], [2, 4]],
+        [[0, 3], [1], [2, 4]],
+        [[0, 3], [1, 4], [2]],
+        [[0], [1, 3, 4], [2]],
+        [[0], [1, 4], [2, 3]],
+        [[0, 2], [1, 4], [3]],
+        [[0], [1, 2, 4], [3]],
+        [[0, 1], [2, 4], [3]],
+        [[0, 1, 4], [2], [3]],
+        [[0, 4], [1, 2], [3]],
+        [[0, 2, 4], [1], [3]],
+        [[0, 4], [1], [2, 3]],
+        [[0, 4], [1, 3], [2]],
+        [[0, 3, 4], [1], [2]]
+    ]
