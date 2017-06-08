3e2babf1e3dd1d53e5f58d4acf070f0a50115af6
Will Mayner <wmayner@gmail.com>
2017-06-08 11:57:26 -0500

    Move partition-related functions to own module

---
+++ pyphi/partition.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# utils.py
+
+"""
+Functions for generating partitions.
+"""
+
+from itertools import product
+
+from .cache import cache
+
+
+def bipartition(a):
+    """Return a list of bipartitions for a sequence.
+
+    Args:
+        a (Iterable): The iterable to partition.
+
+    Returns:
+        list[tuple[tuple]]: A list of tuples containing each of the two
+        partitions.
+
+    Example:
+        >>> bipartition((1,2,3))
+        [((), (1, 2, 3)), ((1,), (2, 3)), ((2,), (1, 3)), ((1, 2), (3,))]
+    """
+    return [(tuple(a[i] for i in part0_idx), tuple(a[j] for j in part1_idx))
+            for part0_idx, part1_idx in bipartition_indices(len(a))]
+
+
+# TODO? [optimization] optimize this to use indices rather than nodes
+# TODO? are native lists really slower
+def directed_bipartition(a):
+    """Return a list of directed bipartitions for a sequence.
+
+    Args:
+        a (Iterable): The iterable to partition.
+
+    Returns:
+        list[tuple[tuple]]: A list of tuples containing each of the two
+        partitions.
+
+    Example:
+        >>> directed_bipartition((1, 2, 3))  # doctest: +NORMALIZE_WHITESPACE
+        [((), (1, 2, 3)),
+         ((1,), (2, 3)),
+         ((2,), (1, 3)),
+         ((1, 2), (3,)),
+         ((3,), (1, 2)),
+         ((1, 3), (2,)),
+         ((2, 3), (1,)),
+         ((1, 2, 3), ())]
+    """
+    return [(tuple(a[i] for i in part0_idx), tuple(a[j] for j in part1_idx))
+            for part0_idx, part1_idx in directed_bipartition_indices(len(a))]
+
+
+# TODO generate these directly
+def directed_bipartition_of_one(a):
+    """Return a list of directed bipartitions for a sequence where each
+    bipartition includes a set of size 1.
+
+    Args:
+        a (Iterable): The iterable to partition.
+
+    Returns:
+        list[tuple[tuple]]: A list of tuples containing each of the two
+        partitions.
+
+    Example:
+        >>> directed_bipartition_of_one((1,2,3))  # doctest: +NORMALIZE_WHITESPACE
+        [((1,), (2, 3)),
+         ((2,), (1, 3)),
+         ((1, 2), (3,)),
+         ((3,), (1, 2)),
+         ((1, 3), (2,)),
+         ((2, 3), (1,))]
+    """
+    return [partition for partition in directed_bipartition(a)
+            if len(partition[0]) == 1 or len(partition[1]) == 1]
+
+
+@cache(cache={}, maxmem=None)
+def directed_bipartition_indices(N):
+    """Return indices for directed bipartitions of a sequence.
+
+    Args:
+        N (int): The length of the sequence.
+
+    Returns:
+        list: A list of tuples containing the indices for each of the two
+        partitions.
+
+    Example:
+        >>> N = 3
+        >>> directed_bipartition_indices(N)  # doctest: +NORMALIZE_WHITESPACE
+        [((), (0, 1, 2)),
+         ((0,), (1, 2)),
+         ((1,), (0, 2)),
+         ((0, 1), (2,)),
+         ((2,), (0, 1)),
+         ((0, 2), (1,)),
+         ((1, 2), (0,)),
+         ((0, 1, 2), ())]
+    """
+    indices = bipartition_indices(N)
+    return indices + [idx[::-1] for idx in indices[::-1]]
+
+
+@cache(cache={}, maxmem=None)
+def bipartition_indices(N):
+    """Return indices for undirected bipartitions of a sequence.
+
+    Args:
+        N (int): The length of the sequence.
+
+    Returns:
+        list: A list of tuples containing the indices for each of the two
+        partitions.
+
+    Example:
+        >>> N = 3
+        >>> bipartition_indices(N)
+        [((), (0, 1, 2)), ((0,), (1, 2)), ((1,), (0, 2)), ((0, 1), (2,))]
+    """
+    result = []
+    if N <= 0:
+        return result
+
+    for i in range(2**(N - 1)):
+        part = [[], []]
+        for n in range(N):
+            bit = (i >> n) & 1
+            part[bit].append(n)
+        result.append((tuple(part[1]), tuple(part[0])))
+    return result
+
+
+@cache(cache={}, maxmem=None)
+def directed_tripartition_indices(N):
+    """Return indices for directed tripartitions of a sequence.
+
+    Args:
+        N (int): The length of the sequence.
+
+    Returns:
+        list[tuple]: A list of tuples containing the indices for each
+            partition.
+
+    Example:
+        >>> N = 1
+        >>> directed_tripartition_indices(N)
+        [((0,), (), ()), ((), (0,), ()), ((), (), (0,))]
+    """
+
+    result = []
+    if N <= 0:
+        return result
+
+    base = [0, 1, 2]
+    for key in product(base, repeat=N):
+        part = [[], [], []]
+        for i, location in enumerate(key):
+            part[location].append(i)
+
+        result.append(tuple(tuple(p) for p in part))
+
+    return result
+
+
+def directed_tripartition(seq):
+    """Generator over all directed tripartitions of a sequence.
+
+    Args:
+        seq (Iterable): a sequence.
+
+    Yields:
+        tuple[tuple]: A tripartition of ``seq``.
+
+    Example:
+        >>> seq = (2, 5)
+        >>> list(directed_tripartition(seq))  # doctest: +NORMALIZE_WHITESPACE
+        [((2, 5), (), ()),
+         ((2,), (5,), ()),
+         ((2,), (), (5,)),
+         ((5,), (2,), ()),
+         ((), (2, 5), ()),
+         ((), (2,), (5,)),
+         ((5,), (), (2,)),
+         ((), (5,), (2,)),
+         ((), (), (2, 5))]
+    """
+    for a, b, c in directed_tripartition_indices(len(seq)):
+        yield (tuple(seq[i] for i in a),
+               tuple(seq[j] for j in b),
+               tuple(seq[k] for k in c))
