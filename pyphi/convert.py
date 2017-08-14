bdddb05a18cb23300b24d1d9ba8a52d39ffb6dd9
Will Mayner <wmayner@gmail.com>
2017-08-14 18:18:19 -0500

    Fix: fix a bug in `convert.to_2_dimensional`
    The resulting array was flipped, e.g. the columns were C, B, A instead
    of A, B, C.

--- pyphi/convert.py
+++ pyphi/convert.py
@@ -198,7 +198,9 @@ def to_2_dimensional(tpm):
     # Get the number of nodes.
     N = tpm.shape[-1]
     # Reshape.
-    return tpm.reshape([2**N, N]).astype(float)
+    tpm = tpm.reshape([2**N, N]).astype(float)
+    # Flip left/right to respect LOLI format.
+    return np.fliplr(tpm)
 
 
 def state_by_state2state_by_node(tpm):
