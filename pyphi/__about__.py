94bc3def4674afbf1bc557d2b6edca2b7d579167
Will Mayner <wmayner@gmail.com>
2017-12-21 17:45:55 -0600

    Bump version to 0.9.1

--- pyphi/__about__.py
+++ pyphi/__about__.py
@@ -5,7 +5,7 @@
 '''PyPhi metadata.'''
 
 __title__ = 'pyphi'
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 __description__ = 'Python library for computing integrated information.'
 __author__ = 'William GP Mayner'
 __author_email__ = 'wmayner@gmail.com'
