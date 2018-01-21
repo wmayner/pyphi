beaaca2f5a1fba25f61c7ee5c56a3502df749df6
Will Mayner <wmayner@gmail.com>
2018-01-21 15:58:38 -0600

    setup.py: Remove redundant classifiers

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
