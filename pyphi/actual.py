cbbb17b040c93c2c66cb40021e7ea6ee1be8422b
Bo Marchman <bo.marchman@gmail.com>
2018-03-08 13:40:04 -0500

    Fix node formatting in AC `repertoire`

--- pyphi/actual.py
+++ pyphi/actual.py
@@ -205,14 +205,15 @@ class Transition:
                 effect repertoire.
         """
         system = self.system[direction]
+        node_labels = system.node_labels
 
         if not set(purview).issubset(self.purview_indices(direction)):
             raise ValueError('{} is not a {} purview in {}'.format(
-                fmt.fmt_mechanism(purview, system), direction, self))
+                fmt.fmt_mechanism(purview, node_labels), direction, self))
 
         if not set(mechanism).issubset(self.mechanism_indices(direction)):
             raise ValueError('{} is no a {} mechanism in {}'.format(
-                fmt.fmt_mechanism(mechanism, system), direction, self))
+                fmt.fmt_mechanism(mechanism, node_labels), direction, self))
 
         return system.repertoire(direction, mechanism, purview)
 
