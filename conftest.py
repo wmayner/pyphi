23d11bf201f2eb6f0f5552255e2cf20ca9a6223c
Bo Marchman <bo.marchman@gmail.com>
2018-04-21 12:53:05 -0500

    Tests: move cache fixtures to top-level `conftest.py`

--- conftest.py
+++ conftest.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
 import os
+import shutil
 
 import pytest
 
 import pyphi
+from pyphi import config, constants, db
+
+
+log = logging.getLogger('pyphi.test')
 
 collect_ignore = [
     "setup.py",
@@ -15,9 +21,9 @@ collect_ignore = [
 git_ignore = os.path.join(os.path.dirname(__file__), '.gitignore')
 collect_ignore += list(filter(None, open(git_ignore).read().split('\n')))
 
+
 # Run slow tests separately with command-line option, filter tests
 # ================================================================
-
 
 def pytest_addoption(parser):
     parser.addoption("--filter", action="store",
@@ -40,6 +46,9 @@ def pytest_runtest_setup(item):
             pytest.skip("need --veryslow option to run")
 
 
+# PyPhi configuration management
+# ================================================================
+
 @pytest.fixture(scope='function')
 def restore_config_afterwards():
     '''Reset PyPhi configuration after a test.
@@ -59,3 +68,64 @@ def disable_progress_bars():
     '''
     with pyphi.config.override(PROGRESS_BARS=False):
         yield
+
+
+# Cache management and fixtures
+# ================================================================
+
+# Use a test database if database caching is enabled.
+if config.CACHING_BACKEND == constants.DATABASE:
+    db.collection = db.database.test
+
+# Backup location for the existing joblib cache directory.
+BACKUP_CACHE_DIR = config.FS_CACHE_DIRECTORY + '.BACKUP'
+
+
+def _flush_joblib_cache():
+    # Remove the old joblib cache directory.
+    shutil.rmtree(config.FS_CACHE_DIRECTORY)
+    # Make a new, empty one.
+    os.mkdir(config.FS_CACHE_DIRECTORY)
+
+
+def _flush_database_cache():
+    # Flush the `test` collection in the database.
+    return db.database.test.remove({})
+
+
+@pytest.fixture
+def flushcache():
+    """Flush the currently enabled cache."""
+    def cache_flusher():
+        log.info("FLUSHING CACHE!")
+        if config.CACHING_BACKEND == constants.DATABASE:
+            _flush_database_cache()
+        elif config.CACHING_BACKEND == constants.FILESYSTEM:
+            _flush_joblib_cache()
+    return cache_flusher
+
+
+@pytest.fixture(scope="session")
+def restore_fs_cache(request):
+    """Temporarily backup, then restore, the user's joblib cache after each
+    testing session."""
+    # Move the joblib cache to a backup location and create a fresh cache if
+    # filesystem caching is enabled
+    if config.CACHING_BACKEND == constants.FILESYSTEM:
+        if os.path.exists(BACKUP_CACHE_DIR):
+            raise Exception("You must move the backup of the filesystem cache "
+                            "at " + BACKUP_CACHE_DIR + " before running the "
+                            "test suite.")
+        shutil.move(config.FS_CACHE_DIRECTORY, BACKUP_CACHE_DIR)
+        os.mkdir(config.FS_CACHE_DIRECTORY)
+
+    def fin():
+        if config.CACHING_BACKEND == constants.FILESYSTEM:
+            # Remove the tests' joblib cache directory.
+            shutil.rmtree(config.FS_CACHE_DIRECTORY)
+            # Restore the old joblib cache.
+            shutil.move(BACKUP_CACHE_DIR,
+                        config.FS_CACHE_DIRECTORY)
+
+    # Restore the cache after the last test with this fixture has run
+    request.addfinalizer(fin)
