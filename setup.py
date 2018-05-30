8b100fa8d4df01f1190b99c2a592f2c769e126d4
Will Mayner <wmayner@gmail.com>
2018-05-30 13:01:26 -0500

    setup.py: Fix classifier

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 4 - Beta;',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
