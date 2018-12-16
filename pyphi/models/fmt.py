c7aebb0e09372c3139172f5de937ec495928fb99
renzocom <renzo.com@gmail.com>
2018-12-15 18:35:38 -0600

    Fix code style and add case in fmt_extended_purview() for single purview

--- pyphi/models/fmt.py
+++ pyphi/models/fmt.py
@@ -427,24 +427,31 @@ def fmt_repertoire(r):
 
 def fmt_extended_purview(extended_purview, node_labels=None):
     """Format an extended purview."""
-    purviews = [fmt_mechanism(purview, node_labels) for purview in extended_purview]
-    return '[' + ', '.join(purviews) + ']'
+    if len(extended_purview)==1:
+        return fmt_mechanism(extended_purview[0], node_labels=node_labels)
+
+    else:
+        purviews = [fmt_mechanism(purview, node_labels=node_labels) for purview in extended_purview]
+        return '[' + ', '.join(purviews) + ']'
+
 
 def fmt_causal_link(causal_link):
     """Format a CausalLink."""
-    return fmt_ac_ria(causal_link, causal_link.extended_purview)
+    return fmt_ac_ria(causal_link, extended_purview=causal_link.extended_purview)
 
 def fmt_ac_ria(ria, extended_purview=None):
     """Format an AcRepertoireIrreducibilityAnalysis."""
     causality = {
-        Direction.CAUSE: (fmt_mechanism(ria.purview, ria.node_labels) if extended_purview is None
-                        else fmt_extended_purview(ria._extended_purview, ria.node_labels),
+        Direction.CAUSE: (fmt_mechanism(ria.purview, ria.node_labels)
+                          if extended_purview is None
+                          else fmt_extended_purview(ria.extended_purview, ria.node_labels),
                           ARROW_LEFT,
                           fmt_mechanism(ria.mechanism, ria.node_labels)),
         Direction.EFFECT: (fmt_mechanism(ria.mechanism, ria.node_labels),
                            ARROW_RIGHT,
-                           fmt_mechanism(ria.purview, ria.node_labels) if extended_purview is None
-                        else fmt_extended_purview(ria._extended_purview, ria.node_labels))
+                           fmt_mechanism(ria.purview, ria.node_labels)
+                           if extended_purview is None
+                           else fmt_extended_purview(ria.extended_purview, ria.node_labels))
     }[ria.direction]
     causality = ' '.join(causality)
 
