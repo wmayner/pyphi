3cc4dcb083ae4c393ccbb1bcd62bd9319f51bfff
Will Mayner <wmayner@gmail.com>
2020-02-07 17:10:11 -0600

    Add support for Python 3.8; drop support for 3.4

--- setup.py
+++ setup.py
@@ -49,10 +49,10 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
     ],
     project_urls={
