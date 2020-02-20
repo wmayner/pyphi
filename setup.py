16bdc7e713e9f4a44168baee24e1085341bd40e3
Will Mayner <wmayner@gmail.com>
2020-02-19 18:38:59 -0600

    Drop support for Python 3.5

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
