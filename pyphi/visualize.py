e4c04bf4ec4c41c7107427ecad10d6d34fac5fed
Will Mayner <wmayner@gmail.com>
2020-07-05 19:05:31 -0500

    visualize: Refine `plot_relations()`
    - Label purviews
    - Handle cases where there are no 2- or 3-relations
    - Color causes and effects differently
    - Scale causes and effects by phi values

--- pyphi/visualize.py
+++ pyphi/visualize.py
@@ -19,9 +19,11 @@ def flatten(iterable):
 
 
 def feature_matrix(ces, relations):
-    """Return a matrix representing each distinction."""
-    # Separate causes from effects
-    ces = rel.separate_ces(ces)
+    """Return a matrix representing each cause and effect in the CES.
+
+    .. note::
+        Assumes that causes and effects have been separated.
+    """
     N = len(ces)
     M = len(relations)
     # Create a mapping from causes and effects to indices in the feature matrix
@@ -62,14 +64,53 @@ def all_edges(vertices):
     return itertools.combinations(vertices, 2)
 
 
-def plot_relations(ces, relations, cause_effect_offset=(0.5, 0, 0), max_order=3):
+def make_label(nodes, node_labels=None):
+    if node_labels is not None:
+        nodes = node_labels.indices2labels(nodes)
+    return "".join(nodes)
+
+
+def label_mechanism(mice):
+    return make_label(mice.mechanism, node_labels=mice.node_labels)
+
+
+def label_purview(mice):
+    return make_label(mice.purview, node_labels=mice.node_labels)
+
+
+def vertex_sizes(min_size, max_size, ces):
+    phis = np.array(
+        [(distinction.cause.phi, distinction.effect.phi) for distinction in ces]
+    )
+    min_phi = phis.min()
+    max_phi = phis.max()
+    return min_size + (((phis - min_phi) * (max_size - min_size)) / (max_phi - min_phi))
+
+
+def plot_relations(
+    ces,
+    relations,
+    max_order=3,
+    cause_effect_offset=(0.5, 0, 0),
+    vertex_size_range=(10, 30),
+):
     # Select only relations <= max_order
-    relations = list(filter(
-        lambda r: len(r.relata) <= max_order,
-        relations
-    ))
+    relations = list(filter(lambda r: len(r.relata) <= max_order, relations))
+    # Separate CES into causes and effects
+    separated_ces = rel.separate_ces(ces)
+
+    # Initialize figure data
+    figure_data = []
+
+    # Dimensionality reduction
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Create the features for each cause/effect based on their relations
-    features = feature_matrix(ces, relations)
+    features = feature_matrix(separated_ces, relations)
+
+    # Now we get one set of coordinates for the CES; these will then be offset to
+    # get coordinates for causes and effects separately, so that causes/effects
+    # are always near each other in the embedding.
+
     # Collapse rows of cause/effect belonging to the same distinction
     # NOTE: This depends on the implementation of `separate_ces`; causes and
     #       effects are assumed to be adjacent in the returned list
@@ -78,67 +119,113 @@ def plot_relations(ces, relations, cause_effect_offset=(0.5, 0, 0), max_order=3)
     # Duplicate causes and effects so they can be plotted separately
     coords = np.empty(
         (distinction_coords.shape[0] * 2, distinction_coords.shape[1]),
-        dtype=distinction_coords.dtype
+        dtype=distinction_coords.dtype,
     )
     coords[0::2] = distinction_coords
     coords[1::2] = distinction_coords
     # Add a small offset to effects to separate them from causes
     coords[1::2] += cause_effect_offset
+
+    # Purviews
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Extract vertex indices for plotly
     x, y, z = coords[:, 0], coords[:, 1], coords[:, 2]
+    # Get mechanism and purview labels
+    mechanism_labels = list(map(label_mechanism, separated_ces))
+    purview_labels = list(map(label_purview, separated_ces))
+    # Make the labels
+    labels = go.Scatter3d(
+        x=x, y=y, z=z, mode="text", text=purview_labels, name="Labels", showlegend=True,
+    )
+    figure_data.append(labels)
+    # Compute size and color
+    size = vertex_sizes(vertex_size_range[0], vertex_size_range[1], ces)
+    color = list(flatten([("red", "green")] * len(ces)))
     vertices = go.Scatter3d(
         x=x,
         y=y,
         z=z,
-        mode='markers+text',
-        text=[''.join([c.node_labels[n] for n in c.mechanism]) for c in ces],
-        name='Vertices',
-        showlegend=True
+        mode="markers",
+        name="Purviews",
+        showlegend=True,
+        marker=dict(size=size, color=color),
     )
+    figure_data.append(vertices)
+
+    # 2-relations
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Get edges from all relations
-    edges = list(flatten(
-        relation_vertex_indices(features, j)
-        for j in range(features.shape[1])
-        if features[:, j].sum() == 2
-    ))
-    # Convert to DataFrame
-    edges = pd.DataFrame(dict(
-        x=x[edges],
-        y=y[edges],
-        z=z[edges],
-        line_group=flatten(zip(range(len(edges)//2), range(len(edges)//2))),
-    ))
-    # Plot edges
-    edge_figure = px.line_3d(
-        edges,
-        x='x',
-        y='y',
-        z='z',
-        line_group='line_group',
+    edges = list(
+        flatten(
+            relation_vertex_indices(features, j)
+            for j in range(features.shape[1])
+            if features[:, j].sum() == 2
+        )
     )
+    if edges:
+        # Convert to DataFrame
+        edges = pd.DataFrame(
+            dict(
+                x=x[edges],
+                y=y[edges],
+                z=z[edges],
+                line_group=flatten(zip(range(len(edges) // 2), range(len(edges) // 2))),
+            )
+        )
+        # Plot edges
+        edge_figure = px.line_3d(edges, x="x", y="y", z="z", line_group="line_group")
+        figure_data.extend(edge_figure.data)
+
+    # 3-relations
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Get triangles from all relations
     triangles = [
         relation_vertex_indices(features, j)
         for j in range(features.shape[1])
         if features[:, j].sum() == 3
     ]
-    # Extract triangle indices
-    i, j, k = zip(*triangles)
-    mesh = go.Mesh3d(
-        # x, y, and z are the coordinates of vertices
-        x=x,
-        y=y,
-        z=z,
-        # i, j, and k are the vertices of triangles
-        i=i,
-        j=j,
-        k=k,
-        # Intensity of each vertex, which will be interpolated and color-coded
-        intensity = np.linspace(0, 1, len(x), endpoint=True),
-        opacity = 0.5,
-        # colorscale='viridis',
-        showscale=False,
-        name='Mesh',
-        showlegend=True
+    if triangles:
+        # Extract triangle indices
+        i, j, k = zip(*triangles)
+        mesh = go.Mesh3d(
+            # x, y, and z are the coordinates of vertices
+            x=x,
+            y=y,
+            z=z,
+            # i, j, and k are the vertices of triangles
+            i=i,
+            j=j,
+            k=k,
+            # Intensity of each vertex, which will be interpolated and color-coded
+            intensity=np.linspace(0, 1, len(x), endpoint=True),
+            opacity=0.2,
+            colorscale="viridis",
+            showscale=False,
+            name="3-Relations",
+            showlegend=True,
+        )
+        figure_data.append(mesh)
+
+    # Create figure
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    axis = dict(
+        showbackground=True,
+        showline=True,
+        zeroline=True,
+        showgrid=True,
+        gridcolor="lightgray",
+        showticklabels=False,
+        title="",
+        showspikes=True,
+        autorange=True,
+        backgroundcolor="white",
     )
-    return go.Figure(data=[vertices, mesh] + list(edge_figure.data))
+    layout = go.Layout(
+        showlegend=True,
+        scene=dict(xaxis=dict(axis), yaxis=dict(axis), zaxis=dict(axis)),
+        hovermode="closest",
+        title="",
+        autosize=True,
+    )
+    # Merge figures
+    return go.Figure(data=figure_data, layout=layout)
