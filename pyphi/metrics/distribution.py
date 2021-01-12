wmayner/pyphi

commit 441a5e8e6483e86e4fc4e5c5c8fa2d4e7f6f0e78

Report maximal state as a tuple

pyphi/metrics/distribution.py
@@ -320,7 +320,7 @@ def maximal_state(repertoire, partitioned_repertoire):
     density = absolute_information_density(
         repertoire.squeeze(), partitioned_repertoire.squeeze()
     )
-    return np.transpose(np.where(density == density.max()))
+    return tuple(np.transpose(np.where(density == density.max())).flat)
 
 
 @measures.register("ID", asymmetric=True)

commit 7b43a338790ec77fe83083593662ad5f4fff9b2d

Report maximal state as array
- Fix bug introduced by 441a5e8e
- Mark tied maximal states in the MICE repr

pyphi/metrics/distribution.py
@@ -320,7 +320,7 @@ def maximal_state(repertoire, partitioned_repertoire):
     density = absolute_information_density(
         repertoire.squeeze(), partitioned_repertoire.squeeze()
     )
-    return tuple(np.transpose(np.where(density == density.max())).flat)
+    return np.transpose(np.where(density == density.max()))
 
 
 @measures.register("ID", asymmetric=True)

pyphi/models/fmt.py
@@ -337,19 +337,19 @@ def fmt_ria(ria, verbose=True, mip=False, purview_state=None):
         mechanism = ""
         direction = ""
 
+    maximal_states = [tuple(state) for state in ria.maximal_state]
+
     # TODO(4.0):  position repertoire and partitioned repertoire side by side
     if config.REPR_VERBOSITY is HIGH:
         partition = "\n{}:\n{}".format(
             ("MIP" if mip else "Partition"), indent(fmt_partition(ria.partition))
         )
         repertoire = "\nRepertoire:\n{}".format(
-            indent(fmt_repertoire(ria.repertoire, mark_states=[ria.maximal_state]))
+            indent(fmt_repertoire(ria.repertoire, mark_states=maximal_states))
         )
         partitioned_repertoire = "\nPartitioned repertoire:\n{}".format(
             indent(
-                fmt_repertoire(
-                    ria.partitioned_repertoire, mark_states=[ria.maximal_state]
-                )
+                fmt_repertoire(ria.partitioned_repertoire, mark_states=maximal_states)
             )
         )
     else: