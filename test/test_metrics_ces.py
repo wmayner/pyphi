8dec08ecc26db159e87f44a4e0db5e55074b518e
Will Mayner <wmayner@gmail.com>
2021-09-19 16:24:38 -0700

    Consolidate `metrics.ces` tests
    And remove duplicate test

--- test/test_metrics_ces.py
+++ test/test_metrics_ces.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 # test/test_metrics_ces.py
 
+from unittest.mock import patch
 import numpy as np
 import pytest
 
-from pyphi import compute, config, metrics
-from pyphi.compute.distance import ces_distance
+from pyphi import compute, config
+from pyphi.metrics.ces import ces_distance, emd_ground_distance
+from pyphi import models
 
 
 def test_emd_ground_distance_must_be_symmetric():
@@ -14,7 +16,7 @@ def test_emd_ground_distance_must_be_symmetric():
     b = np.ones((2, 2, 2)) / 8
     with config.override(REPERTOIRE_DISTANCE="KLD"):
         with pytest.raises(ValueError):
-            metrics.ces.emd_ground_distance(a, b)
+            emd_ground_distance(a, b)
 
 
 def test_ces_distances(s):
@@ -22,10 +24,10 @@ def test_ces_distances(s):
         sia = compute.subsystem.sia(s)
 
     with config.override(CES_DISTANCE="EMD"):
-        assert compute.distance.ces_distance(sia.ces, sia.partitioned_ces) == 2.3125
+        assert ces_distance(sia.ces, sia.partitioned_ces) == 2.3125
 
     with config.override(CES_DISTANCE="SUM_SMALL_PHI"):
-        assert compute.distance.ces_distance(sia.ces, sia.partitioned_ces) == 1.083333
+        assert ces_distance(sia.ces, sia.partitioned_ces) == 1.083333
 
 
 def test_sia_uses_ces_distances(s):
@@ -36,3 +38,45 @@ def test_sia_uses_ces_distances(s):
     with config.override(REPERTOIRE_DISTANCE="EMD", CES_DISTANCE="SUM_SMALL_PHI"):
         sia = compute.subsystem.sia(s)
         assert sia.phi == 1.083333
+
+
+@patch("pyphi.metrics.ces._emd_simple")
+@patch("pyphi.metrics.ces._emd")
+def test_ces_distance_uses_simple_vs_emd(mock_emd_distance, mock_simple_distance, s):
+    """Quick check that we use the correct EMD distance function for CESs.
+
+    If the two CESs differ only in that some concepts have
+    moved to the null concept and all other concepts are the same then
+    we use the simple CES distance. Otherwise, use the EMD.
+    """
+    mock_emd_distance.return_value = float()
+    mock_simple_distance.return_value = float()
+
+    make_mice = lambda: models.MaximallyIrreducibleCauseOrEffect(
+        models.RepertoireIrreducibilityAnalysis(
+            phi=0,
+            direction=None,
+            mechanism=None,
+            purview=None,
+            partition=None,
+            repertoire=None,
+            partitioned_repertoire=None,
+        )
+    )
+
+    lone_concept = models.Concept(
+        cause=make_mice(), effect=make_mice(), mechanism=(0, 1), subsystem=s
+    )
+    # lone concept -> null concept
+    ces_distance((lone_concept,), ())
+    assert mock_emd_distance.called is False
+    assert mock_simple_distance.called is True
+    mock_simple_distance.reset_mock()
+
+    other_concept = models.Concept(
+        cause=make_mice(), effect=make_mice(), mechanism=(0, 1, 2), subsystem=s
+    )
+    # different concepts in CES
+    ces_distance((lone_concept,), (other_concept,))
+    assert mock_emd_distance.called is True
+    assert mock_simple_distance.called is False
