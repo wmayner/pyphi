f30dc988c4bb72b576de961a983025fb1a46f457
Will Mayner <wmayner@gmail.com>
2021-09-20 12:28:24 -0500

    Bump version to 1.2.1

--- pyphi/__about__.py
+++ pyphi/__about__.py
@@ -4,16 +4,24 @@
 
 """PyPhi metadata."""
 
-__title__ = 'pyphi'
-__version__ = '1.2.0'
-__description__ = 'Python library for computing integrated information.'
-__author__ = 'William GP Mayner'
-__author_email__ = 'wmayner@gmail.com'
-__author_website__ = 'http://willmayner.com'
-__copyright__ = 'Copyright 2014-2019 William GP Mayner'
-__license__ = 'GNU General Public License v3.0'
-__url__ = 'http://github.com/wmayner/pyphi'
+__title__ = "pyphi"
+__version__ = "1.2.1"
+__description__ = "Python library for computing integrated information."
+__author__ = "William GP Mayner"
+__author_email__ = "wmayner@gmail.com"
+__author_website__ = "http://willmayner.com"
+__copyright__ = "Copyright 2014-2019 William GP Mayner"
+__license__ = "GNU General Public License v3.0"
+__url__ = "http://github.com/wmayner/pyphi"
 
-__all__ = ['__title__', '__version__', '__description__', '__author__',
-           '__author_email__', '__author_website__', '__copyright__',
-           '__license__', '__url__']
+__all__ = [
+    "__title__",
+    "__version__",
+    "__description__",
+    "__author__",
+    "__author_email__",
+    "__author_website__",
+    "__copyright__",
+    "__license__",
+    "__url__",
+]
