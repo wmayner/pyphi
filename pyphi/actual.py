500c4e64fb45d18baaadbf8c39b887aa911d6b7c
Will Mayner <wmayner@gmail.com>
2022-05-04 15:56:46 -0500

    Move AC measure registry to metrics/distribution

--- pyphi/actual.py
+++ pyphi/actual.py
@@ -27,7 +27,7 @@ import numpy as np
 from . import config, connectivity, constants, compute, exceptions, utils, validate
 from .compute import parallel
 from .direction import Direction
-from .metrics.actual import measures
+from .metrics.distribution import actual_causation_measures as measures
 from .models import (
     Account,
     AcRepertoireIrreducibilityAnalysis,
