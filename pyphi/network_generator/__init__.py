3075ad08f876486164497e55bf5cdcaf03dd8760
Will Mayner <wmayner@gmail.com>
2022-08-03 16:59:01 -0500

    network_generator: Fix CM logic

--- pyphi/network_generator/__init__.py
+++ pyphi/network_generator/__init__.py
@@ -53,5 +53,5 @@ def build_network(
     if node_labels is None:
         node_labels = string.ascii_uppercase[: weights.shape[0]]
     tpm = build_tpm(unit_function, weights, **kwargs)
-    cm = weights.astype(int)
+    cm = (weights > 0.0).astype(int)
     return Network(tpm, cm=cm, node_labels=node_labels)
