515f1bfdb0af219dc3179545c3e32eb52f4cf577
Will Mayner <wmayner@gmail.com>
2022-08-05 16:38:30 -0500

    network_generator: Allow heterogeneous unit funcs

--- pyphi/network_generator/__init__.py
+++ pyphi/network_generator/__init__.py
@@ -2,9 +2,12 @@
 # network_generator/__init__.py
 
 import string
+from typing import Callable, Iterable, Union
 
 import numpy as np
+from numpy.typing import ArrayLike
 
+from ..labels import NodeLabels
 from ..network import Network
 from ..utils import all_states
 from . import ising, utils, weights
@@ -14,27 +17,53 @@ def logical_or_function(element, weights, state):
     return utils.input_weight(element, weights, state) >= 1
 
 
+def logical_and_function(element, weights, state):
+    # Convention: i,j means i -> j
+    num_inputs = (weights[:, element] > 1).sum()
+    return utils.input_weight(element, weights, state) >= num_inputs
+
+
+def logical_parity_function(element, weights, state):
+    return utils.input_weight(element, weights, state) % 2 >= 1
+
+
 UNIT_FUNCTIONS = {
     "ising": ising.probability,
     "or": logical_or_function,
+    "and": logical_and_function,
+    "parity": logical_parity_function,
 }
 
 
-def build_tpm(unit_function, weights, **kwargs):
+def build_tpm(
+    unit_functions: Union[Callable, Iterable[Callable]], weights: ArrayLike, **kwargs
+):
     if weights.ndim != 2 or weights.shape[0] != weights.shape[1]:
         raise ValueError("weights must be a square matrix")
+
     N = weights.shape[0]
+
+    if isinstance(unit_functions, Iterable):
+        unit_functions = list(unit_functions)
+        if len(unit_functions) != weights.shape[0]:
+            raise ValueError(
+                "Number of unit functions must match number of nodes in weight "
+                "matrix"
+            )
+    else:
+        unit_functions = [unit_functions] * N
+
     tpm = np.zeros([2] * N + [N])
     for state in all_states(N):
-        for element in range(N):
-            tpm[state + (element,)] = unit_function(element, weights, state, **kwargs)
+        for element, func in enumerate(unit_functions):
+            tpm[state + (element,)] = func(element, weights, state, **kwargs)
     return tpm
 
 
 def build_network(
-    unit_function,
-    weights,
-    node_labels=None,
+    unit_functions: Union[Callable, Iterable[Callable]],
+    weights: ArrayLike,
+    node_labels: NodeLabels = None,
     **kwargs,
 ):
     """Returns a PyPhi network given a weight matrix and a unit function.
@@ -52,6 +81,6 @@ def build_network(
     """
     if node_labels is None:
         node_labels = string.ascii_uppercase[: weights.shape[0]]
-    tpm = build_tpm(unit_function, weights, **kwargs)
+    tpm = build_tpm(unit_functions, weights, **kwargs)
     cm = (weights > 0.0).astype(int)
     return Network(tpm, cm=cm, node_labels=node_labels)
