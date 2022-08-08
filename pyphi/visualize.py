47b07f40693ad6b775a7100bd68daf490633bc8c
Will Mayner <wmayner@gmail.com>
2022-08-08 15:49:34 -0500

    visualize: Add distribution plotting

--- pyphi/visualize.py
+++ pyphi/visualize.py
@@ -1,17 +1,23 @@
+import string
 from collections import defaultdict
 from collections.abc import Iterable
 from dataclasses import dataclass, field
 from itertools import combinations
-from math import cos, isclose, radians, sin
+from math import cos, isclose, log2, radians, sin
 from typing import Callable, Mapping, Optional, Union
 
 import networkx as nx
 import numpy as np
+import pandas as pd
 import plotly.colors
 import scipy.special
+import seaborn as sb
 from _plotly_utils.basevalidators import ColorscaleValidator
+from matplotlib import pyplot as plt
 from plotly import graph_objs as go
 from toolz import partition
+
+import pyphi
 
 from . import config
 from .big_phi import PhiStructure
@@ -770,3 +776,123 @@ def plot_subsystem(subsystem):
     )
     return g, fig
 
+
+###############################################################################
+# Distributions
+###############################################################################
+
+
+def all_states_str(*args, **kwargs):
+    """Return all states as bit strings."""
+    for state in pyphi.utils.all_states(*args, **kwargs):
+        yield "".join(map(str, state))
+
+
+def _plot_distribution_bar(data, ax, label, **kwargs):
+    sb.barplot(data=data, x="state", y="probability", ax=ax, **kwargs)
+
+    plt.xticks(rotation=90, ha="center", va="top")
+    # Add state label
+    xtick_pad = 6
+    xtick_length = 6
+    ax.tick_params(axis="x", pad=xtick_pad, length=xtick_length)
+    ax.annotate(
+        str(label) if label is not None else "",
+        xy=(-0.5, 0),
+        xycoords="data",
+        xytext=(0, -(xtick_pad + xtick_length)),
+        textcoords="offset points",
+        annotation_clip=False,
+        rotation=90,
+        ha="right",
+        va="top",
+    )
+
+    return ax
+
+
+def _plot_distribution_line(data, ax, **kwargs):
+    sb.lineplot(data=data, x="state", y="probability", ax=ax, **kwargs)
+    return ax
+
+
+def plot_distribution(
+    *distributions,
+    states=None,
+    label=None,
+    figsize=(9, 3),
+    fig=None,
+    ax=None,
+    lineplot_threshold=64,
+    title="State distribution",
+    y_label="Pr(state)",
+    validate=True,
+    labels=None,
+    **kwargs,
+):
+    """Plot a distribution over states.
+
+    Arguments:
+        d (array_like): The distribution. If no states are provided, must
+            have length equal to a power of 2. Multidimensional distributions
+            are flattened with ``pyphi.distribution.flatten()``.
+
+    Keyword Arguments:
+        states (Iterable | None): The states corresponding to the
+            probabilities in the distribution; if ``None``, infers states from
+            the length of the distribution and assumes little-endian ordering.
+        **kwargs: Passed to ``sb.barplot()``.
+    """
+    if validate and not all(np.allclose(d.sum(), 1, rtol=1e-4) for d in distributions):
+        raise ValueError("a distribution does not sum to 1!")
+    if validate and not all(
+        (distributions[0].index == d.index).all() for d in distributions
+    ):
+        raise ValueError("distribution indices do not match")
+
+    # defaults = dict(color="#666")
+    defaults = dict()
+    # Overrride defaults with keyword arguments
+    kwargs = {**defaults, **kwargs}
+
+    if fig is None and ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+    if fig is None:
+        fig = plt.gcf()
+    if ax is None:
+        ax = plt.gca()
+
+    distributions = [pyphi.distribution.flatten(d) for d in distributions]
+    d = distributions[0]
+
+    N = log2(np.prod(d.shape))
+    if states is None:
+        if N.is_integer() and len(d) <= lineplot_threshold:
+            N = int(N)
+            states = list(all_states_str(N))
+            if label is None:
+                label = string.ascii_uppercase[:N]
+        else:
+            states = np.arange(len(d))
+
+    if labels is None:
+        labels = list(map(str, range(len(distributions))))
+
+    data = pd.concat(
+        [
+            pd.DataFrame(dict(probability=d, state=states, hue=[label] * len(d)))
+            for d, label in zip(distributions, labels)
+        ]
+    )
+
+    if len(d) > lineplot_threshold:
+        ax = _plot_distribution_line(data, ax, hue="hue", **kwargs)
+    else:
+        ax = _plot_distribution_bar(data, ax, label, hue="hue", **kwargs)
+
+    ax.set_title(title)
+    ax.set_ylabel(y_label, labelpad=12)
+    ax.set_xlabel("state", labelpad=12)
+    ax.legend(bbox_to_anchor=(1.1, 1.05))
+
+    return fig, ax
