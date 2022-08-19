0616b3d26c48912350fcc318bd037c365815e76c
Will Mayner <wmayner@gmail.com>
2022-08-19 11:50:24 -0500

    partition: SET_UNI/BI: Include complete

--- pyphi/partition.py
+++ pyphi/partition.py
@@ -840,6 +840,7 @@ def general_bidirectional(node_indices, node_labels=None):
 
 def _unidirectional_set_partitions(node_indices, node_labels=None):
     """Generate all unidirectional set partitions of a set of nodes."""
+    yield CompleteGeneralKCut(node_indices, node_labels=node_labels)
     _node_indices = set(node_indices)
     for partition in partitions(node_indices, nontrivial=True):
         for directions in product(Direction.all(), repeat=len(partition)):
