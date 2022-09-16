4679bd72fa90274e540fe9f60daf0434b3ce0deb
Will Mayner <wmayner@gmail.com>
2022-09-16 16:36:20 -0500

    visualize: Refactor `plot_subsystem` into `plot_graph`

--- pyphi/visualize.py
+++ pyphi/visualize.py
@@ -756,26 +756,28 @@ NODE_COLORS = {
 }
 
 
-def plot_subsystem(subsystem):
-    # TODO highlight nodes in subsystem
-    g = nx.from_numpy_matrix(subsystem.cm, create_using=nx.DiGraph)
-    nx.relabel_nodes(
-        g, dict(zip(range(subsystem.network.size), subsystem.node_labels)), copy=False
-    )
-    node_colors = [
-        NODE_COLORS[(i in subsystem.node_indices, subsystem.state[i])]
-        for i in range(subsystem.network.size)
-    ]
-    fig = nx.draw(
+def plot_graph(g, **kwargs):
+    nx.draw(
         g,
         with_labels=True,
         arrowsize=20,
         node_size=600,
         font_color="white",
-        node_color=node_colors,
+        **kwargs,
     )
-    return g, fig
 
+def plot_subsystem(subsystem, **kwargs):
+    g = nx.from_numpy_matrix(subsystem.cm, create_using=nx.DiGraph)
+    nx.relabel_nodes(
+        g, dict(zip(range(subsystem.network.size), subsystem.node_labels)), copy=False
+    )
+    if 'node_color' not in kwargs:
+        kwargs['node_color'] = [
+            NODE_COLORS[(i in subsystem.node_indices, subsystem.state[i])]
+            for i in range(subsystem.network.size)
+        ]
+    plot_graph(g, **kwargs)
+    return g
 
 ###############################################################################
 # Distributions
