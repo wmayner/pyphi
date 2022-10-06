4a25e329511cefb65933729a1fcd1c3b303003d8
David Viggiano <dviggiano@wisc.edu>
2022-10-06 11:11:24 -0500

    Create sample CES test data and use same examples

--- test/test_new_big_phi.py
+++ test/test_new_big_phi.py
@@ -13,14 +13,13 @@ from pyphi.jsonify import jsonify
 from pyphi.new_big_phi import sia
 from pyphi.compute.subsystem import ces
 
-example_sia_subsystems = ["basic", "basic_noisy_selfloop", "fig4", "grid3", "xor"]
-example_ces_subsystems = ["basic", "fig4", "fig5a", "fig5b", "grid3", "rule110", "xor"]
+example_subsystems = ["basic", "basic_noisy_selfloop", "fig4", "grid3", "xor"]
 
 @pytest.fixture
 def expected_sia():
     cases = {}
     
-    for example in example_sia_subsystems:
+    for example in example_subsystems:
         with open(f"test/data/sia/sia_{example}.json") as f:
             cases[example] = json.load(f)
     
@@ -30,7 +29,7 @@ def expected_sia():
 def expected_ces():
     cases = {}
     
-    for example in example_ces_subsystems:
+    for example in example_subsystems:
         with open(f"test/data/ces/ces_{example}.json") as f:
             cases[example] = json.load(f)
     
@@ -41,7 +40,7 @@ def expected_ces():
 
 @pytest.mark.parametrize(
     "example_subsystem", # TODO more parameters
-    example_sia_subsystems
+    example_subsystems
 )
 def test_sia(example_subsystem, expected_sia):
     example_func = EXAMPLES["subsystem"][example_subsystem]
@@ -57,29 +56,19 @@ def test_sia(example_subsystem, expected_sia):
 
 @pytest.mark.parametrize(
     "example_subsystem", # TODO more parameters
-    example_ces_subsystems
+    example_subsystems
 )
 def test_compute_subsystem_ces(example_subsystem, expected_ces):
     example_func = EXAMPLES["subsystem"][example_subsystem]
     actual_ces = ces(example_func())
     
     # convert CES object to JSON format
-    actual_ces = copy(actual_ces.__dict__)
     jsonify(actual_ces)
     
-    for key, value in actual_ces.items():
-        actual_ces[key] = jsonify(value)
+    del actual_ces["node_labels"]["__id__"]
+    del actual_ces[example_subsystem]["node_labels"]["__id__"]
     
-    assert actual_ces.keys() == expected_ces[example_subsystem].keys()
-        
-    for key, value in expected_ces[example_subsystem].items():
-        # ignore node_labels.__id__
-        if key == "node_labels":
-            for attr in value:
-                if attr != "__id__":
-                    assert actual_ces[key][attr] == value[attr]
-        else:
-            assert actual_ces[key] == expected_ces[example_subsystem][key]
+    assert actual_ces == expected_ces[example_subsystem]
 
 def test_relations():
-    assert False == True # TODO
+    assert False # TODO
