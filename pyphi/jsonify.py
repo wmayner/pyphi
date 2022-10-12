9bb42d1696663b2f0e6a9c3a365917948a458112
David Viggiano <dviggiano@wisc.edu>
2022-10-12 13:42:10 -0500

    jsonify.jsonify(): Recurse over data in frozensets

--- pyphi/jsonify.py
+++ pyphi/jsonify.py
@@ -149,8 +149,8 @@ def jsonify(obj):  # pylint: disable=too-many-return-statements
     if hasattr(obj, "__dict__"):
         return _jsonify_dict(obj.__dict__)
 
-    # Recurse over lists, tuples, and sets.
-    if isinstance(obj, (list, tuple, set)):
+    # Recurse over lists, tuples, sets, and frozensets.
+    if isinstance(obj, (list, tuple, set, frozenset)):
         return [jsonify(item) for item in obj]
 
     # Otherwise, give up and hope it's serializable.
