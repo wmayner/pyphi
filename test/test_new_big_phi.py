e6119360b3fa089aaff9444038943dd606680734
David Viggiano <dviggiano@wisc.edu>
2022-10-12 13:54:22 -0500

    test_new_big_phi(): Add test_relations

--- test/test_new_big_phi.py
+++ test/test_new_big_phi.py
@@ -11,11 +11,12 @@ from pyphi.examples import EXAMPLES
 from pyphi.jsonify import jsonify
 from pyphi.new_big_phi import sia
 from pyphi.compute.subsystem import ces
+from pyphi.relations import relations
 
 NETWORKS = ["basic", "basic_noisy_selfloop", "fig4", "grid3", "xor"]
 
 def expected_sia(example):
-    SIA_PATH = f"test/data/sia/sia_{example}.json"
+    SIA_PATH = f"test/data/new_big_phi/sia/sia_{example}.json"
     
     with open(SIA_PATH) as f:
         expected = json.load(f)
@@ -23,9 +24,17 @@ def expected_sia(example):
     return expected
 
 def expected_ces(example):
-    CES_PATH = f"test/data/ces/ces_{example}.json"
+    CES_PATH = f"test/data/new_big_phi/ces/ces_{example}.json"
     
     with open(CES_PATH) as f:
+        expected = json.load(f)
+    
+    return expected
+
+def expected_relations(example):
+    RELATIONS_PATH = f"test/data/new_big_phi/relations/relations_{example}.json"
+    
+    with open(RELATIONS_PATH) as f:
         expected = json.load(f)
     
     return expected
@@ -64,5 +73,17 @@ def test_compute_subsystem_ces(case_name):
     
     assert actual == expected
 
-def test_phi_structure_match(example_network):
-    assert False # TODO
+# TODO failing via PyTest, but passing in notebook
+@pytest.mark.parametrize(
+    "case_name",  # TODO more parameters
+    NETWORKS
+)
+def test_relations(case_name):  # TODO more descriptive name
+    subsystem = EXAMPLES["subsystem"][case_name]()
+    ces_obj = ces(subsystem)
+    actual = relations(subsystem, ces_obj, parallel=False)
+    expected = expected_relations(case_name)
+    
+    actual = jsonify(actual)
+    
+    assert actual == expected
