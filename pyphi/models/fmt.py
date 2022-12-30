19849ceeaa63b7212ec3868dff92bf5561f90c59
Will Mayner <wmayner@gmail.com>
2022-12-30 15:58:02 -0600

    fmt: Format MICE with `str` in distinctions

--- pyphi/models/fmt.py
+++ pyphi/models/fmt.py
@@ -522,15 +522,10 @@ def fmt_concept(concept):
     """Format a |Concept|."""
 
     def fmt_cause_or_effect(x):  # pylint: disable=missing-docstring
-        return box(
-            indent(
-                fmt_ria(x.ria, verbose=False, mip=True),
-                amount=1,
-            )
-        )
+        return box(indent(str(x), amount=1))
 
-    cause = header("MIC", fmt_cause_or_effect(concept.cause))
-    effect = header("MIE", fmt_cause_or_effect(concept.effect))
+    cause = fmt_cause_or_effect(concept.cause)
+    effect = fmt_cause_or_effect(concept.effect)
     ce = side_by_side(cause, effect)
 
     mechanism = fmt_mechanism(concept.mechanism, concept.node_labels)
