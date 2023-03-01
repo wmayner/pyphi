f2be2d422f017e355172ed10d75784612683f048
David Viggiano <dviggiano@wisc.edu>
2023-03-01 14:17:35 -0600

    test_network(): Add test_build_cm_implicit_tpm()

--- test/test_network.py
+++ test/test_network.py
@@ -61,21 +61,21 @@ def test_potential_purviews(s):
 
 def test_node_labels(standard):
     labels = ("A", "B", "C")
-    network = Network(standard.tpm.tpm, node_labels=labels)
+    network = Network(standard.tpm, node_labels=labels)
     assert network.node_labels.labels == labels
 
     labels = ("A", "B")  # Too few labels
     with pytest.raises(ValueError):
-        Network(standard.tpm.tpm, node_labels=labels)
+        Network(standard.tpm, node_labels=labels)
 
     # Auto-generated labels
-    network = Network(standard.tpm.tpm, node_labels=None)
+    network = Network(standard.tpm, node_labels=None)
     assert network.node_labels.labels == ("n0", "n1", "n2")
 
 
 def test_num_states(standard):
     assert standard.num_states == 8
-
+    
 
 def test_repr(standard):
     print(repr(standard))
@@ -91,3 +91,75 @@ def test_len(standard):
 
 def test_size(standard):
     assert standard.size == 3
+
+
+def test_build_cm_implicit_tpm():
+    # no CM
+    tpm = [
+        np.array([
+            [
+                [
+                    [0., 1.],
+                    [1., 0.]
+                ]
+            ],
+            [
+                [
+                    [1., 0.],
+                    [0., 1.]
+                ]
+            ]
+        ]),
+        np.array([
+            [
+                [
+                    [1., 0.],
+                    [1., 0.]
+                ],
+                [
+                    [0., 1.],
+                    [1., 0.]
+                ]
+            ],
+            [
+                [
+                    [0., 1.],
+                    [0., 1.]
+                ],
+                [
+                    [0., 1.],
+                    [1., 0.]
+                ]
+            ]
+        ]),
+        np.array([
+            [
+                [
+                    [1., 0.],
+                    [1., 0.]
+                ],
+                [
+                    [0., 1.],
+                    [0., 1.]
+                ]
+            ]
+        ])
+    ]
+    cm = np.array([
+        [1, 1, 0],
+        [0, 1, 1],
+        [1, 1, 1]
+    ])
+    network = Network(tpm)
+    assert((network.cm == cm).all())
+    # correct CM
+    network = Network(tpm, cm)
+    assert((network.cm == cm).all())
+    # incorrect CM
+    cm = np.array([
+        [1, 0, 0],
+        [1, 1, 0],
+        [1, 1, 1]
+    ])
+    with pytest.raises(ValueError):
+        network = Network(tpm, cm)
