1a6d7ecd483e8838e8cad21882835b8ed41af1b7
David Viggiano <dviggiano@wisc.edu>
2023-03-01 14:11:33 -0600

    _build_cm(): Should use cm param, not self.cm

--- pyphi/network.py
+++ pyphi/network.py
@@ -180,7 +180,7 @@ class Network:
                             cm[j][i] = 1
         else:
             for i, shape in enumerate(shapes):
-                for j, val in enumerate(self.cm[..., i]):
+                for j, val in enumerate(cm[..., i]):
                     if (val == 0 and shape[j] != 1) or (val != 0 and shape[j] == 1):
                         raise ValueError(f"Node shape of {shape[j]} does not correspond to connectivity matrix.")
             
